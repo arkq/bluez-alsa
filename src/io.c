arkq/bluez-alsa

commit 79f68a9a9bc1482ef4d0014b83812061dd8a4c72

Minimize audio delay and increase responsiveness
Due to this change some audio tearing might occur. However, it will be
caused by the "unresponsiveness" of the BlueZ stack and the Bluetooth
device itself. It happens mostly on the first transport acquisition -
just after the device is connected.

src/transport.c
@@ -12,10 +12,13 @@
 #include "transport.h"
 
 #include <errno.h>
+#include <fcntl.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/eventfd.h>
+#include <sys/socket.h>
+#include <sys/types.h>
 
 #include <bluetooth/bluetooth.h>
 #include <bluetooth/hci.h>
@@ -699,6 +702,15 @@ int transport_acquire_bt_a2dp(struct ba_transport *t) {
 	fd_list = g_dbus_message_get_unix_fd_list(rep);
 	t->bt_fd = g_unix_fd_list_get(fd_list, 0, &err);
 	t->release = transport_release_bt_a2dp;
+
+	/* Minimize audio delay and increase responsiveness (seeking, stopping) by
+	 * decreasing the BT socket output buffer. We will use a tripled write MTU
+	 * value, in order to prevent tearing due to temporal heavy load. Also,
+	 * make socket IO blocking, so we won't bother about partial writes. */
+	size_t size = t->mtu_write * 3;
+	if (setsockopt(t->bt_fd, SOL_SOCKET, SO_SNDBUF, &size, sizeof(size)) == -1)
+		warn("Couldn't set socket output buffer size: %s", strerror(errno));
+	fcntl(t->bt_fd, F_SETFL, fcntl(t->bt_fd, F_GETFL) & ~O_NONBLOCK);
 
 	debug("New transport: %d (MTU: R:%zu W:%zu)", t->bt_fd, t->mtu_read, t->mtu_write);
 

commit 6879322faddab1d8835c511ac85c0add735679fb

Restore non-blocking mode for BT socket
It seems that commit 79f68a9 has introduced a very peculiar bug. In
that commit I've changed socket mode from non-blocking to blocking
and decreased socket buffer. It has turned out, that changing BT
socket mode to blocking causes some kind of a "dead-lock" during
writing to this socket...

Fixes #79

src/io.c
@@ -1,6 +1,6 @@
 /*
  * BlueALSA - io.c
- * Copyright (c) 2016-2017 Arkadiusz Bokowy
+ * Copyright (c) 2016-2018 Arkadiusz Bokowy
  *
  * This file is a part of bluez-alsa.
  *
@@ -177,6 +177,26 @@ static ssize_t io_thread_write_pcm(struct ba_pcm *pcm, const int16_t *buffer, si
 
 	/* It is guaranteed, that this function will write data atomically. */
 	return samples;
+}
+
+/**
+ * Write data to the BT SEQPACKET socket. */
+static ssize_t io_thread_write_bt(int fd, const uint8_t *buffer, size_t len) {
+
+	struct pollfd pfds[] = {{ fd, POLLOUT, 0 }};
+	ssize_t ret;
+
+retry:
+	if ((ret = write(fd, buffer, len)) == -1)
+		switch (errno) {
+		case EINTR:
+			goto retry;
+		case EAGAIN:
+			poll(pfds, sizeof(pfds) / sizeof(*pfds), -1);
+			goto retry;
+		}
+
+	return ret;
 }
 
 void *io_thread_a2dp_sink_sbc(void *arg) {
@@ -493,14 +513,18 @@ void *io_thread_a2dp_source_sbc(void *arg) {
 			rtp_header->timestamp = htonl(timestamp);
 			rtp_payload->frame_count = sbc_frames;
 
-			if (write(t->bt_fd, out_buffer, output - out_buffer) == -1) {
+			pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, NULL);
+
+			if (io_thread_write_bt(t->bt_fd, out_buffer, output - out_buffer) == -1) {
 				if (errno == ECONNRESET || errno == ENOTCONN) {
-					/* exit the thread upon BT socket disconnection */
-					debug("BT socket disconnected");
+					/* exit thread upon BT socket disconnection */
+					debug("BT socket disconnected: %d", t->bt_fd);
 					goto fail;
 				}
 				error("BT socket write error: %s", strerror(errno));
 			}
+
+			pthread_setcancelstate(PTHREAD_CANCEL_DISABLE, NULL);
 
 			/* keep data transfer at a constant bit rate, also
 			 * get a timestamp for the next RTP frame */
@@ -926,15 +950,19 @@ void *io_thread_a2dp_source_aac(void *arg) {
 					rtp_header->markbit = len < payload_len_max;
 					rtp_header->seq_number = htons(++seq_number);
 
-					if ((ret = write(t->bt_fd, out_buffer, rtp_header_len + len)) == -1) {
+					pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, NULL);
+
+					if ((ret = io_thread_write_bt(t->bt_fd, out_buffer, rtp_header_len + len)) == -1) {
 						if (errno == ECONNRESET || errno == ENOTCONN) {
-							/* exit the thread upon BT socket disconnection */
-							debug("BT socket disconnected");
+							/* exit thread upon BT socket disconnection */
+							debug("BT socket disconnected: %d", t->bt_fd);
 							goto fail;
 						}
 						error("BT socket write error: %s", strerror(errno));
 						break;
 					}
+
+					pthread_setcancelstate(PTHREAD_CANCEL_DISABLE, NULL);
 
 					/* break if the last part of the payload has been written */
 					if ((payload_len -= ret - rtp_header_len) == 0)
@@ -1113,14 +1141,18 @@ void *io_thread_a2dp_source_aptx(void *arg) {
 
 			}
 
-			if (write(t->bt_fd, out_buffer, output - out_buffer) == -1) {
+			pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, NULL);
+
+			if (io_thread_write_bt(t->bt_fd, out_buffer, output - out_buffer) == -1) {
 				if (errno == ECONNRESET || errno == ENOTCONN) {
-					/* exit the thread upon BT socket disconnection */
-					debug("BT socket disconnected");
+					/* exit thread upon BT socket disconnection */
+					debug("BT socket disconnected: %d", t->bt_fd);
 					goto fail;
 				}
 				error("BT socket write error: %s", strerror(errno));
 			}
+
+			pthread_setcancelstate(PTHREAD_CANCEL_DISABLE, NULL);
 
 			/* keep data transfer at a constant bit rate */
 			asrsync_sync(&asrs, pcm_frames);

src/transport.c
@@ -12,7 +12,6 @@
 #include "transport.h"
 
 #include <errno.h>
-#include <fcntl.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
@@ -814,12 +813,10 @@ int transport_acquire_bt_a2dp(struct ba_transport *t) {
 
 	/* Minimize audio delay and increase responsiveness (seeking, stopping) by
 	 * decreasing the BT socket output buffer. We will use a tripled write MTU
-	 * value, in order to prevent tearing due to temporal heavy load. Also,
-	 * make socket IO blocking, so we won't bother about partial writes. */
+	 * value, in order to prevent tearing due to temporal heavy load. */
 	size_t size = t->mtu_write * 3;
 	if (setsockopt(t->bt_fd, SOL_SOCKET, SO_SNDBUF, &size, sizeof(size)) == -1)
 		warn("Couldn't set socket output buffer size: %s", strerror(errno));
-	fcntl(t->bt_fd, F_SETFL, fcntl(t->bt_fd, F_GETFL) & ~O_NONBLOCK);
 
 	debug("New transport: %d (MTU: R:%zu W:%zu)", t->bt_fd, t->mtu_read, t->mtu_write);
 