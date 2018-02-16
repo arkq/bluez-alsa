arkq/bluez-alsa

commit 99d9d72a5aed22dd755485a2661e209ded697079

Wait for internal buffers to be drained
When ALSA client requests PCM drain, wait until our internal buffer
is consumed. Note, that samples might still be dropped (not played by
the Bluetooth device), because there is not possible to drain BlueZ
internal buffers (there is no API for that). One possible solution is
to releasing A2DP transport asynchronously with some arbitrary delay.

Fixes #67

src/io.c
@@ -415,7 +415,12 @@ void *io_thread_a2dp_source_sbc(void *arg) {
 		/* add PCM socket to the poll if transport is active */
 		pfds[1].fd = t->state == TRANSPORT_ACTIVE ? t->a2dp.pcm.fd : -1;
 
-		if (poll(pfds, sizeof(pfds) / sizeof(*pfds), -1) == -1) {
+		switch (poll(pfds, sizeof(pfds) / sizeof(*pfds), 100)) {
+		case 0:
+			if (t->a2dp.pcm.sync)
+				pthread_cond_signal(&t->a2dp.pcm.drained);
+			continue;
+		case -1:
 			error("Transport poll error: %s", strerror(errno));
 			goto fail;
 		}
@@ -852,7 +857,12 @@ void *io_thread_a2dp_source_aac(void *arg) {
 		/* add PCM socket to the poll if transport is active */
 		pfds[1].fd = t->state == TRANSPORT_ACTIVE ? t->a2dp.pcm.fd : -1;
 
-		if (poll(pfds, sizeof(pfds) / sizeof(*pfds), -1) == -1) {
+		switch (poll(pfds, sizeof(pfds) / sizeof(*pfds), 100)) {
+		case 0:
+			if (t->a2dp.pcm.sync)
+				pthread_cond_signal(&t->a2dp.pcm.drained);
+			continue;
+		case -1:
 			error("Transport poll error: %s", strerror(errno));
 			goto fail;
 		}
@@ -1031,7 +1041,12 @@ void *io_thread_a2dp_source_aptx(void *arg) {
 		/* add PCM socket to the poll if transport is active */
 		pfds[1].fd = t->state == TRANSPORT_ACTIVE ? t->a2dp.pcm.fd : -1;
 
-		if (poll(pfds, sizeof(pfds) / sizeof(*pfds), -1) == -1) {
+		switch (poll(pfds, sizeof(pfds) / sizeof(*pfds), 100)) {
+		case 0:
+			if (t->a2dp.pcm.sync)
+				pthread_cond_signal(&t->a2dp.pcm.drained);
+			continue;
+		case -1:
 			error("Transport poll error: %s", strerror(errno));
 			goto fail;
 		}
@@ -1190,7 +1205,12 @@ void *io_thread_sco(void *arg) {
 		if (t->sco.mic_pcm.fd == -1)
 			pfds[1].fd = -1;
 
-		if (poll(pfds, sizeof(pfds) / sizeof(*pfds), -1) == -1) {
+		switch (poll(pfds, sizeof(pfds) / sizeof(*pfds), 100)) {
+		case 0:
+			if (t->sco.spk_pcm.sync)
+				pthread_cond_signal(&t->sco.spk_pcm.drained);
+			continue;
+		case -1:
 			error("Transport poll error: %s", strerror(errno));
 			goto fail;
 		}

src/transport.c
@@ -285,6 +285,8 @@ struct ba_transport *transport_new_a2dp(
 
 	t->a2dp.pcm.fd = -1;
 	t->a2dp.pcm.client = -1;
+	pthread_cond_init(&t->a2dp.pcm.drained, NULL);
+	pthread_mutex_init(&t->a2dp.pcm.drained_mn, NULL);
 
 	bluealsa_event();
 	return t;
@@ -316,8 +318,13 @@ struct ba_transport *transport_new_rfcomm(
 
 	t_sco->sco.spk_pcm.fd = -1;
 	t_sco->sco.spk_pcm.client = -1;
+	pthread_cond_init(&t_sco->sco.spk_pcm.drained, NULL);
+	pthread_mutex_init(&t_sco->sco.spk_pcm.drained_mn, NULL);
+
 	t_sco->sco.mic_pcm.fd = -1;
 	t_sco->sco.mic_pcm.client = -1;
+	pthread_cond_init(&t_sco->sco.mic_pcm.drained, NULL);
+	pthread_mutex_init(&t_sco->sco.mic_pcm.drained_mn, NULL);
 
 	transport_set_state(t_sco, TRANSPORT_ACTIVE);
 
@@ -362,6 +369,8 @@ void transport_free(struct ba_transport *t) {
 	switch (t->type) {
 	case TRANSPORT_TYPE_A2DP:
 		transport_release_pcm(&t->a2dp.pcm);
+		pthread_cond_destroy(&t->a2dp.pcm.drained);
+		pthread_mutex_destroy(&t->a2dp.pcm.drained_mn);
 		free(t->a2dp.cconfig);
 		break;
 	case TRANSPORT_TYPE_RFCOMM:
@@ -371,7 +380,11 @@ void transport_free(struct ba_transport *t) {
 		break;
 	case TRANSPORT_TYPE_SCO:
 		transport_release_pcm(&t->sco.spk_pcm);
+		pthread_cond_destroy(&t->sco.spk_pcm.drained);
+		pthread_mutex_destroy(&t->sco.spk_pcm.drained_mn);
 		transport_release_pcm(&t->sco.mic_pcm);
+		pthread_cond_destroy(&t->sco.mic_pcm.drained);
+		pthread_mutex_destroy(&t->sco.mic_pcm.drained_mn);
 		t->sco.rfcomm->rfcomm.sco = NULL;
 		break;
 	}
@@ -711,20 +724,65 @@ int transport_set_state_from_string(struct ba_transport *t, const char *state) {
 	return 0;
 }
 
+int transport_drain_pcm(struct ba_transport *t) {
+
+	struct ba_pcm *pcm = NULL;
+
+	switch (t->profile) {
+	case BLUETOOTH_PROFILE_NULL:
+	case BLUETOOTH_PROFILE_A2DP_SINK:
+		break;
+	case BLUETOOTH_PROFILE_A2DP_SOURCE:
+		pcm = &t->a2dp.pcm;
+		break;
+	case BLUETOOTH_PROFILE_HSP_AG:
+	case BLUETOOTH_PROFILE_HFP_AG:
+		pcm = &t->sco.spk_pcm;
+		break;
+	case BLUETOOTH_PROFILE_HSP_HS:
+	case BLUETOOTH_PROFILE_HFP_HF:
+		break;
+	}
+
+	if (pcm == NULL || t->state != TRANSPORT_ACTIVE)
+		return 0;
+
+	pthread_mutex_lock(&pcm->drained_mn);
+
+	pcm->sync = true;
+	pthread_cond_wait(&pcm->drained, &pcm->drained_mn);
+	pcm->sync = false;
+
+	pthread_mutex_unlock(&pcm->drained_mn);
+
+	/* TODO: Asynchronous transport release.
+	 *
+	 * Unfortunately, BlueZ does not provide API for internal buffer drain.
+	 * Also, there is no specification for Bluetooth playback drain. In order
+	 * to make sure, that all samples are played out, we have to wait some
+	 * arbitrary time before releasing transport. In order to make it right,
+	 * there is a requirement for an asynchronous release mechanism, which
+	 * is not implemented - it requires a little bit of refactoring. */
+	usleep(200000);
+
+	debug("PCM drained");
+	return 0;
+}
+
 int transport_acquire_bt_a2dp(struct ba_transport *t) {
 
 	GDBusMessage *msg, *rep;
 	GUnixFDList *fd_list;
 	GError *err = NULL;
 
-	msg = g_dbus_message_new_method_call(t->dbus_owner, t->dbus_path, "org.bluez.MediaTransport1",
-			t->state == TRANSPORT_PENDING ? "TryAcquire" : "Acquire");
-
 	if (t->bt_fd != -1) {
 		warn("Closing dangling BT socket: %d", t->bt_fd);
 		close(t->bt_fd);
 		t->bt_fd = -1;
 	}
+
+	msg = g_dbus_message_new_method_call(t->dbus_owner, t->dbus_path, "org.bluez.MediaTransport1",
+			t->state == TRANSPORT_PENDING ? "TryAcquire" : "Acquire");
 
 	if ((rep = g_dbus_connection_send_message_with_reply_sync(config.dbus, msg,
 					G_DBUS_SEND_MESSAGE_FLAGS_NONE, -1, NULL, NULL, &err)) == NULL)

src/asound/bluealsa-pcm.c
@@ -342,8 +342,9 @@ static int bluealsa_prepare(snd_pcm_ioplug_t *io) {
 }
 
 static int bluealsa_drain(snd_pcm_ioplug_t *io) {
-	(void)io;
-	debug("Draining");
+	struct bluealsa_pcm *pcm = io->private_data;
+	if (bluealsa_drain_transport(pcm->fd, pcm->transport) == -1)
+		return -errno;
 	return 0;
 }
 

src/ctl.c
@@ -455,6 +455,9 @@ static void ctl_thread_cmd_pcm_control(const struct request *req, int fd) {
 	case COMMAND_PCM_RESUME:
 		transport_set_state(t, TRANSPORT_ACTIVE);
 		break;
+	case COMMAND_PCM_DRAIN:
+		transport_drain_pcm(t);
+		break;
 	case COMMAND_PCM_READY:
 		break;
 	default:
@@ -483,6 +486,7 @@ static void *ctl_thread(void *arg) {
 		[COMMAND_PCM_CLOSE] = ctl_thread_cmd_pcm_close,
 		[COMMAND_PCM_PAUSE] = ctl_thread_cmd_pcm_control,
 		[COMMAND_PCM_RESUME] = ctl_thread_cmd_pcm_control,
+		[COMMAND_PCM_DRAIN] = ctl_thread_cmd_pcm_control,
 		[COMMAND_PCM_READY] = ctl_thread_cmd_pcm_control,
 	};
 

src/shared/ctl-client.c
@@ -392,3 +392,28 @@ int bluealsa_pause_transport(int fd, const struct msg_transport *transport, bool
 
 	return bluealsa_send_request(fd, &req);
 }
+
+/**
+ * Drain PCM transport.
+ *
+ * @param fd Opened socket file descriptor.
+ * @param transport Address to the transport structure with the addr, type
+ *   and stream fields set - other fields are not used by this function.
+ * @return Upon success this function returns 0. Otherwise, -1 is returned. */
+int bluealsa_drain_transport(int fd, const struct msg_transport *transport) {
+
+	struct request req = {
+		.command = COMMAND_PCM_DRAIN,
+		.addr = transport->addr,
+		.type = transport->type,
+		.stream = transport->stream,
+	};
+
+#if DEBUG
+	char addr_[18];
+	ba2str_(&req.addr, addr_);
+	debug("Requesting PCM drain for %s", addr_);
+#endif
+
+	return bluealsa_send_request(fd, &req);
+}

src/shared/ctl-client.h
@@ -29,5 +29,6 @@ int bluealsa_get_transport_delay(int fd, const struct msg_transport *transport);
 int bluealsa_open_transport(int fd, const struct msg_transport *transport);
 int bluealsa_close_transport(int fd, const struct msg_transport *transport);
 int bluealsa_pause_transport(int fd, const struct msg_transport *transport, bool pause);
+int bluealsa_drain_transport(int fd, const struct msg_transport *transport);
 
 #endif

src/shared/ctl-proto.h
@@ -33,6 +33,7 @@ enum command {
 	COMMAND_PCM_CLOSE,
 	COMMAND_PCM_PAUSE,
 	COMMAND_PCM_RESUME,
+	COMMAND_PCM_DRAIN,
 	COMMAND_PCM_READY,
 	__COMMAND_MAX
 };

src/transport.h
@@ -81,6 +81,11 @@ struct ba_pcm {
 	 * by the PCM client lookup function - transport_lookup_pcm_client() */
 	int client;
 
+	/* variables used for PCM synchronization */
+	bool sync;
+	pthread_cond_t drained;
+	pthread_mutex_t drained_mn;
+
 };
 
 struct ba_transport {
@@ -228,6 +233,8 @@ int transport_set_volume(struct ba_transport *t, uint8_t ch1_muted, uint8_t ch2_
 
 int transport_set_state(struct ba_transport *t, enum ba_transport_state state);
 int transport_set_state_from_string(struct ba_transport *t, const char *state);
+
+int transport_drain_pcm(struct ba_transport *t);
 
 int transport_acquire_bt_a2dp(struct ba_transport *t);
 int transport_release_bt_a2dp(struct ba_transport *t);

commit 3e600b874d4424e01fe3d803a993f4982f8f33df

Drain optimization - use timeout only when needed
This commit also fixes a bug introduced in the 99d9d72. Since then it
was impossible to exit (cleanly and without breaking bluealsa server)
SCO thread with a player which uses snd_pcm_drain() call.

src/io.c
@@ -355,6 +355,7 @@ void *io_thread_a2dp_source_sbc(void *arg) {
 	int16_t *in_buffer_tail = in_buffer;
 	size_t in_samples = in_buffer_size / sizeof(int16_t);
 
+	int poll_timeout = -1;
 	struct asrsync asrs = { .frames = 0 };
 	struct pollfd pfds[] = {
 		{ t->event_fd, POLLIN, 0 },
@@ -371,10 +372,10 @@ void *io_thread_a2dp_source_sbc(void *arg) {
 		/* add PCM socket to the poll if transport is active */
 		pfds[1].fd = t->state == TRANSPORT_ACTIVE ? t->a2dp.pcm.fd : -1;
 
-		switch (poll(pfds, sizeof(pfds) / sizeof(*pfds), 100)) {
+		switch (poll(pfds, sizeof(pfds) / sizeof(*pfds), poll_timeout)) {
 		case 0:
-			if (t->a2dp.pcm.sync)
-				pthread_cond_signal(&t->a2dp.pcm.drained);
+			pthread_cond_signal(&t->a2dp.pcm.drained);
+			poll_timeout = -1;
 			continue;
 		case -1:
 			error("Transport poll error: %s", strerror(errno));
@@ -386,6 +387,8 @@ void *io_thread_a2dp_source_sbc(void *arg) {
 			eventfd_t event;
 			eventfd_read(pfds[0].fd, &event);
 			asrs.frames = 0;
+			if (t->a2dp.pcm.sync)
+				poll_timeout = 100;
 			continue;
 		}
 
@@ -795,6 +798,7 @@ void *io_thread_a2dp_source_aac(void *arg) {
 	size_t in_samples = in_buffer_size / in_buffer_element_size;
 	in_buffer_tail = in_buffer;
 
+	int poll_timeout = -1;
 	struct asrsync asrs = { .frames = 0 };
 	struct pollfd pfds[] = {
 		{ t->event_fd, POLLIN, 0 },
@@ -811,10 +815,10 @@ void *io_thread_a2dp_source_aac(void *arg) {
 		/* add PCM socket to the poll if transport is active */
 		pfds[1].fd = t->state == TRANSPORT_ACTIVE ? t->a2dp.pcm.fd : -1;
 
-		switch (poll(pfds, sizeof(pfds) / sizeof(*pfds), 100)) {
+		switch (poll(pfds, sizeof(pfds) / sizeof(*pfds), poll_timeout)) {
 		case 0:
-			if (t->a2dp.pcm.sync)
-				pthread_cond_signal(&t->a2dp.pcm.drained);
+			pthread_cond_signal(&t->a2dp.pcm.drained);
+			poll_timeout = -1;
 			continue;
 		case -1:
 			error("Transport poll error: %s", strerror(errno));
@@ -826,6 +830,8 @@ void *io_thread_a2dp_source_aac(void *arg) {
 			eventfd_t event;
 			eventfd_read(pfds[0].fd, &event);
 			asrs.frames = 0;
+			if (t->a2dp.pcm.sync)
+				poll_timeout = 100;
 			continue;
 		}
 
@@ -978,6 +984,7 @@ void *io_thread_a2dp_source_aptx(void *arg) {
 	int16_t *in_buffer_tail = in_buffer;
 	size_t in_samples = in_buffer_size / sizeof(int16_t);
 
+	int poll_timeout = -1;
 	struct asrsync asrs = { .frames = 0 };
 	struct pollfd pfds[] = {
 		{ t->event_fd, POLLIN, 0 },
@@ -994,10 +1001,10 @@ void *io_thread_a2dp_source_aptx(void *arg) {
 		/* add PCM socket to the poll if transport is active */
 		pfds[1].fd = t->state == TRANSPORT_ACTIVE ? t->a2dp.pcm.fd : -1;
 
-		switch (poll(pfds, sizeof(pfds) / sizeof(*pfds), 100)) {
+		switch (poll(pfds, sizeof(pfds) / sizeof(*pfds), poll_timeout)) {
 		case 0:
-			if (t->a2dp.pcm.sync)
-				pthread_cond_signal(&t->a2dp.pcm.drained);
+			pthread_cond_signal(&t->a2dp.pcm.drained);
+			poll_timeout = -1;
 			continue;
 		case -1:
 			error("Transport poll error: %s", strerror(errno));
@@ -1009,6 +1016,8 @@ void *io_thread_a2dp_source_aptx(void *arg) {
 			eventfd_t event;
 			eventfd_read(pfds[0].fd, &event);
 			asrs.frames = 0;
+			if (t->a2dp.pcm.sync)
+				poll_timeout = 100;
 			continue;
 		}
 
@@ -1126,6 +1135,7 @@ void *io_thread_sco(void *arg) {
 		goto fail;
 	}
 
+	int poll_timeout = -1;
 	struct asrsync asrs = { .frames = 0 };
 	struct pollfd pfds[] = {
 		{ t->event_fd, POLLIN, 0 },
@@ -1162,10 +1172,10 @@ void *io_thread_sco(void *arg) {
 		if (t->sco.mic_pcm.fd == -1)
 			pfds[1].fd = -1;
 
-		switch (poll(pfds, sizeof(pfds) / sizeof(*pfds), 100)) {
+		switch (poll(pfds, sizeof(pfds) / sizeof(*pfds), poll_timeout)) {
 		case 0:
-			if (t->sco.spk_pcm.sync)
-				pthread_cond_signal(&t->sco.spk_pcm.drained);
+			pthread_cond_signal(&t->sco.spk_pcm.drained);
+			poll_timeout = -1;
 			continue;
 		case -1:
 			error("Transport poll error: %s", strerror(errno));
@@ -1179,6 +1189,15 @@ void *io_thread_sco(void *arg) {
 
 			eventfd_t event;
 			eventfd_read(pfds[0].fd, &event);
+
+			/* FIXME: Drain functionality for speaker.
+			 * XXX: Right now it is not possible to drain speaker PCM (in a clean
+			 *      fashion), because poll() will not timeout if we've got incoming
+			 *      data from the microphone (BT SCO socket). In order not to hang
+			 *      forever in the transport_drain_pcm() function, we will signal
+			 *      PCM drain right now. */
+			if (t->sco.spk_pcm.sync)
+				pthread_cond_signal(&t->sco.spk_pcm.drained);
 
 			const enum hfp_ind *inds = t->sco.rfcomm->rfcomm.hfp_inds;
 			bool release = false;
@@ -1321,6 +1340,8 @@ retry_sco_write:
 		}
 		else if (pfds[3].revents & (POLLERR | POLLHUP)) {
 			debug("PCM poll error status: %#x", pfds[3].revents);
+			close(t->sco.spk_pcm.fd);
+			t->sco.spk_pcm.fd = -1;
 		}
 
 		if (pfds[4].revents & POLLOUT) {

src/transport.c
@@ -760,6 +760,7 @@ int transport_drain_pcm(struct ba_transport *t) {
 	pthread_mutex_lock(&pcm->drained_mn);
 
 	pcm->sync = true;
+	eventfd_write(t->event_fd, 1);
 	pthread_cond_wait(&pcm->drained, &pcm->drained_mn);
 	pcm->sync = false;
 