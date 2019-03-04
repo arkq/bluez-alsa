arkq/bluez-alsa

commit 0afd2023f6e9d5fca12df12b3369918c8cfdb682

Keep transport profile and type in one field
Keeping transport type and profile as a separate field adds unnecessary
data redundancy. We can keep exactly the same bit of information in one
field. Such composition should improve readability and allow hash-map
optimization in the future.

src/bluez.h
@@ -1,6 +1,6 @@
 /*
  * BlueALSA - bluez.h
- * Copyright (c) 2016-2017 Arkadiusz Bokowy
+ * Copyright (c) 2016-2019 Arkadiusz Bokowy
  *
  * This file is a part of bluez-alsa.
  *
@@ -18,16 +18,6 @@
 #define BLUETOOTH_UUID_HSP_AG      "00001112-0000-1000-8000-00805F9B34FB"
 #define BLUETOOTH_UUID_HFP_HF      "0000111E-0000-1000-8000-00805F9B34FB"
 #define BLUETOOTH_UUID_HFP_AG      "0000111F-0000-1000-8000-00805F9B34FB"
-
-enum bluetooth_profile {
-	BLUETOOTH_PROFILE_NULL = 0,
-	BLUETOOTH_PROFILE_A2DP_SOURCE,
-	BLUETOOTH_PROFILE_A2DP_SINK,
-	BLUETOOTH_PROFILE_HSP_HS,
-	BLUETOOTH_PROFILE_HSP_AG,
-	BLUETOOTH_PROFILE_HFP_HF,
-	BLUETOOTH_PROFILE_HFP_AG,
-};
 
 void bluez_register_a2dp(void);
 void bluez_register_hfp(void);

src/ctl.c
@@ -28,7 +28,6 @@
 #include "a2dp-codecs.h"
 #include "bluealsa.h"
 #include "bluez-iface.h"
-#include "bluez.h"
 #include "hfp.h"
 #include "transport.h"
 #include "utils.h"
@@ -86,21 +85,19 @@ static int ctl_lookup_transport(GHashTable *devices, const bdaddr_t *addr,
 			case BA_PCM_TYPE_NULL:
 				continue;
 			case BA_PCM_TYPE_A2DP:
-				if ((*t)->type != TRANSPORT_TYPE_A2DP)
-					continue;
 				if (type & BA_PCM_STREAM_PLAYBACK &&
-						(*t)->profile == BLUETOOTH_PROFILE_A2DP_SOURCE)
+						(*t)->type.profile == BA_TRANSPORT_PROFILE_A2DP_SOURCE)
 					return 0;
 				if (type & BA_PCM_STREAM_CAPTURE &&
-						(*t)->profile == BLUETOOTH_PROFILE_A2DP_SINK)
+						(*t)->type.profile == BA_TRANSPORT_PROFILE_A2DP_SINK)
 					return 0;
 				continue;
 			case BA_PCM_TYPE_SCO:
-				if ((*t)->type == TRANSPORT_TYPE_SCO)
+				if ((*t)->type.profile & BA_TRANSPORT_PROFILE_MASK_SCO)
 					return 0;
 				continue;
 			case BA_PCM_TYPE_RFCOMM:
-				if ((*t)->type == TRANSPORT_TYPE_RFCOMM)
+				if ((*t)->type.profile == BA_TRANSPORT_PROFILE_RFCOMM)
 					return 0;
 				continue;
 			}
@@ -119,22 +116,16 @@ static int ctl_lookup_transport(GHashTable *devices, const bdaddr_t *addr,
  * @return On success address of the PCM structure is returned. If the PCM
  *   structure can not be determined, NULL is returned. */
 static struct ba_pcm *ctl_lookup_pcm(struct ba_transport *t, uint8_t type, int client) {
-	switch (t->type) {
-	case TRANSPORT_TYPE_A2DP:
+	if (t->type.profile & BA_TRANSPORT_PROFILE_MASK_A2DP)
 		if (t->a2dp.pcm.client == client)
 			return &t->a2dp.pcm;
-		break;
-	case TRANSPORT_TYPE_RFCOMM:
-		debug("Trying to get PCM for RFCOMM transport... that's nuts");
-		break;
-	case TRANSPORT_TYPE_SCO:
+	if (t->type.profile & BA_TRANSPORT_PROFILE_MASK_SCO) {
 		if (type & BA_PCM_STREAM_PLAYBACK)
 			if (t->sco.spk_pcm.client == client)
 				return &t->sco.spk_pcm;
 		if (type & BA_PCM_STREAM_CAPTURE)
 			if (t->sco.mic_pcm.client == client)
 				return &t->sco.mic_pcm;
-		break;
 	}
 	return NULL;
 }
@@ -143,34 +134,30 @@ static struct ba_msg_transport *ctl_transport(const struct ba_transport *t,
 		struct ba_msg_transport *transport) {
 
 	bacpy(&transport->addr, &t->device->addr);
+	transport->type = BA_PCM_TYPE_NULL;
+	transport->codec = t->type.codec;
+	transport->channels = transport_get_channels(t);
+	transport->sampling = transport_get_sampling(t);
+	transport->delay += t->delay;
 
-	switch (t->type) {
-	case TRANSPORT_TYPE_A2DP:
-		transport->type = BA_PCM_TYPE_A2DP | (t->profile == BLUETOOTH_PROFILE_A2DP_SOURCE ?
+	if (t->type.profile & BA_TRANSPORT_PROFILE_MASK_A2DP) {
+		transport->type = BA_PCM_TYPE_A2DP | (t->type.profile == BA_TRANSPORT_PROFILE_A2DP_SOURCE ?
 				BA_PCM_STREAM_PLAYBACK : BA_PCM_STREAM_CAPTURE);
 		transport->ch1_muted = t->a2dp.ch1_muted;
 		transport->ch1_volume = t->a2dp.ch1_volume;
 		transport->ch2_muted = t->a2dp.ch2_muted;
 		transport->ch2_volume = t->a2dp.ch2_volume;
 		transport->delay = t->a2dp.delay;
-		break;
-	case TRANSPORT_TYPE_RFCOMM:
-		transport->type = BA_PCM_TYPE_NULL;
-		break;
-	case TRANSPORT_TYPE_SCO:
+	}
+
+	if (t->type.profile & BA_TRANSPORT_PROFILE_MASK_SCO) {
 		transport->type = BA_PCM_TYPE_SCO | BA_PCM_STREAM_PLAYBACK | BA_PCM_STREAM_CAPTURE;
 		transport->ch1_muted = t->sco.spk_muted;
 		transport->ch1_volume = t->sco.spk_gain;
 		transport->ch2_muted = t->sco.mic_muted;
 		transport->ch2_volume = t->sco.mic_gain;
 		transport->delay = 10;
-		break;
 	}
-
-	transport->codec = t->codec;
-	transport->channels = transport_get_channels(t);
-	transport->sampling = transport_get_sampling(t);
-	transport->delay += t->delay;
 
 	return transport;
 }
@@ -354,7 +341,8 @@ static void ctl_thread_cmd_pcm_open(struct ba_ctl *ctl, struct ba_request *req, 
 
 	pthread_mutex_lock(&t->mutex);
 
-	if (t->type == TRANSPORT_TYPE_SCO && t->codec == HFP_CODEC_UNDEFINED) {
+	if (t->type.profile & BA_TRANSPORT_PROFILE_MASK_SCO &&
+			t->type.codec == HFP_CODEC_UNDEFINED) {
 		status.code = BA_STATUS_CODE_CODEC_NOT_SELECTED;
 		goto final;
 	}
@@ -411,7 +399,7 @@ static void ctl_thread_cmd_pcm_open(struct ba_ctl *ctl, struct ba_request *req, 
 	 * is about to be transfered. It is most likely, that BT headset will not
 	 * run voltage converter (power-on its circuit board) until the transport
 	 * is acquired - in order to extend battery life. */
-	if (t->profile == BLUETOOTH_PROFILE_A2DP_SOURCE)
+	if (t->type.profile == BA_TRANSPORT_PROFILE_A2DP_SOURCE)
 		if (t->acquire(t) == -1) {
 			status.code = BA_STATUS_CODE_ERROR_UNKNOWN;
 			goto fail;
@@ -564,17 +552,14 @@ static void *ctl_thread(void *arg) {
 					g_hash_table_iter_init(&iter_d, config.devices);
 					while (g_hash_table_iter_next(&iter_d, NULL, (gpointer)&d)) {
 						g_hash_table_iter_init(&iter_t, d->transports);
-						while (g_hash_table_iter_next(&iter_t, NULL, (gpointer)&t))
-							switch (t->type) {
-							case TRANSPORT_TYPE_RFCOMM:
-								continue;
-							case TRANSPORT_TYPE_A2DP:
+						while (g_hash_table_iter_next(&iter_t, NULL, (gpointer)&t)) {
+							if (t->type.profile & BA_TRANSPORT_PROFILE_MASK_A2DP) {
 								if (t->a2dp.pcm.client == fd) {
 									transport_release_pcm(&t->a2dp.pcm);
 									transport_send_signal(t, TRANSPORT_PCM_CLOSE);
 								}
-								continue;
-							case TRANSPORT_TYPE_SCO:
+							}
+							if (t->type.profile & BA_TRANSPORT_PROFILE_MASK_SCO) {
 								if (t->sco.spk_pcm.client == fd) {
 									transport_release_pcm(&t->sco.spk_pcm);
 									transport_send_signal(t, TRANSPORT_PCM_CLOSE);
@@ -584,6 +569,7 @@ static void *ctl_thread(void *arg) {
 									transport_send_signal(t, TRANSPORT_PCM_CLOSE);
 								}
 							}
+						}
 					}
 
 					bluealsa_devpool_mutex_unlock();

src/rfcomm.c
@@ -275,7 +275,7 @@ static int rfcomm_handler_brsf_set_cb(struct rfcomm_conn *c, const struct bt_at 
 	/* Codec negotiation is not supported in the HF, hence no
 	 * wideband audio support. AT+BAC will not be sent. */
 	if (!(t->rfcomm.hfp_features & HFP_HF_FEAT_CODEC))
-		t->rfcomm.sco->codec = HFP_CODEC_CVSD;
+		t->rfcomm.sco->type.codec = HFP_CODEC_CVSD;
 
 	sprintf(tmp, "%u", config.hfp.features_rfcomm_ag);
 	if (rfcomm_write_at(fd, AT_TYPE_RESP, "+BRSF", tmp) == -1)
@@ -298,7 +298,7 @@ static int rfcomm_handler_brsf_resp_cb(struct rfcomm_conn *c, const struct bt_at
 
 	/* codec negotiation is not supported in the AG */
 	if (!(t->rfcomm.hfp_features & HFP_AG_FEAT_CODEC))
-		t->rfcomm.sco->codec = HFP_CODEC_CVSD;
+		t->rfcomm.sco->type.codec = HFP_CODEC_CVSD;
 
 	if (c->state < HFP_SLC_BRSF_SET)
 		rfcomm_set_hfp_state(c, HFP_SLC_BRSF_SET);
@@ -354,8 +354,8 @@ static int rfcomm_handler_bcs_set_cb(struct rfcomm_conn *c, const struct bt_at *
 	struct ba_transport * const t = c->t;
 	const int fd = t->bt_fd;
 
-	if (t->rfcomm.sco->codec != atoi(at->value)) {
-		warn("Codec not acknowledged: %d != %s", t->rfcomm.sco->codec, at->value);
+	if (t->rfcomm.sco->type.codec != atoi(at->value)) {
+		warn("Codec not acknowledged: %d != %s", t->rfcomm.sco->type.codec, at->value);
 		if (rfcomm_write_at(fd, AT_TYPE_RESP, NULL, "ERROR") == -1)
 			return -1;
 		return 0;
@@ -390,7 +390,7 @@ static int rfcomm_handler_bcs_resp_cb(struct rfcomm_conn *c, const struct bt_at 
 	struct ba_transport * const t = c->t;
 	const int fd = t->bt_fd;
 
-	t->rfcomm.sco->codec = atoi(at->value);
+	t->rfcomm.sco->type.codec = atoi(at->value);
 	if (rfcomm_write_at(fd, AT_TYPE_CMD_SET, "+BCS", at->value) == -1)
 		return -1;
 
@@ -572,8 +572,7 @@ void *rfcomm_thread(void *arg) {
 		{ t->bt_fd, POLLIN, 0 },
 	};
 
-	debug("Starting RFCOMM loop: %s",
-			bluetooth_profile_to_string(t->profile));
+	debug("Starting loop: %s", ba_transport_type_to_string(t->type));
 	for (;;) {
 
 		/* During normal operation, RFCOMM should block indefinitely. However,
@@ -603,7 +602,7 @@ void *rfcomm_thread(void *arg) {
 				goto ioerror;
 			}
 
-			if (t->profile == BLUETOOTH_PROFILE_HFP_HF)
+			if (t->type.profile == BA_TRANSPORT_PROFILE_HFP_HF)
 				switch (conn.state) {
 				case HFP_DISCONNECTED:
 					sprintf(tmp, "%u", config.hfp.features_rfcomm_hf);
@@ -662,7 +661,7 @@ void *rfcomm_thread(void *arg) {
 							BA_PCM_TYPE_SCO | BA_PCM_STREAM_PLAYBACK | BA_PCM_STREAM_CAPTURE);
 				}
 
-			if (t->profile == BLUETOOTH_PROFILE_HFP_AG)
+			if (t->type.profile == BA_TRANSPORT_PROFILE_HFP_AG)
 				switch (conn.state) {
 				case HFP_DISCONNECTED:
 				case HFP_SLC_BRSF_SET:
@@ -680,7 +679,7 @@ void *rfcomm_thread(void *arg) {
 					if (t->rfcomm.hfp_features & HFP_HF_FEAT_CODEC) {
 						if (rfcomm_write_at(pfds[1].fd, AT_TYPE_RESP, "+BCS", "1") == -1)
 							goto ioerror;
-						t->rfcomm.sco->codec = HFP_CODEC_CVSD;
+						t->rfcomm.sco->type.codec = HFP_CODEC_CVSD;
 						conn.handler = &rfcomm_handler_bcs_set;
 						break;
 					}

commit fe3fb65b5d158c5412c993748a16468034b0c8eb

Fix RFCOMM thread bug introduced in 0afd202

src/ba-adapter.c
@@ -12,6 +12,7 @@
 
 #include <errno.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "bluealsa.h"
 

src/ba-transport.c
@@ -40,63 +40,54 @@ static int io_thread_create(struct ba_transport *t) {
 	void *(*routine)(void *) = NULL;
 	int ret;
 
-	switch (t->type.profile) {
-	case BA_TRANSPORT_PROFILE_A2DP_SOURCE:
-			switch (t->type.codec) {
-			case A2DP_CODEC_SBC:
-				routine = io_thread_a2dp_source_sbc;
-				break;
+	if (t->type.profile & BA_TRANSPORT_PROFILE_RFCOMM)
+		routine = rfcomm_thread;
+	else if (t->type.profile & BA_TRANSPORT_PROFILE_MASK_SCO)
+		routine = io_thread_sco;
+	else if (t->type.profile & BA_TRANSPORT_PROFILE_A2DP_SOURCE)
+		switch (t->type.codec) {
+		case A2DP_CODEC_SBC:
+			routine = io_thread_a2dp_source_sbc;
+			break;
 #if ENABLE_MPEG
-			case A2DP_CODEC_MPEG12:
-				break;
+		case A2DP_CODEC_MPEG12:
+			break;
 #endif
 #if ENABLE_AAC
-			case A2DP_CODEC_MPEG24:
-				routine = io_thread_a2dp_source_aac;
-				break;
+		case A2DP_CODEC_MPEG24:
+			routine = io_thread_a2dp_source_aac;
+			break;
 #endif
 #if ENABLE_APTX
-			case A2DP_CODEC_VENDOR_APTX:
-				routine = io_thread_a2dp_source_aptx;
-				break;
+		case A2DP_CODEC_VENDOR_APTX:
+			routine = io_thread_a2dp_source_aptx;
+			break;
 #endif
 #if ENABLE_LDAC
-			case A2DP_CODEC_VENDOR_LDAC:
-				routine = io_thread_a2dp_source_ldac;
-				break;
+		case A2DP_CODEC_VENDOR_LDAC:
+			routine = io_thread_a2dp_source_ldac;
+			break;
 #endif
-			default:
-				warn("Codec not supported: %u", t->type.codec);
-			}
-		break;
-	case BA_TRANSPORT_PROFILE_A2DP_SINK:
-			switch (t->type.codec) {
-			case A2DP_CODEC_SBC:
-				routine = io_thread_a2dp_sink_sbc;
-				break;
+		default:
+			warn("Codec not supported: %u", t->type.codec);
+		}
+	else if (t->type.profile & BA_TRANSPORT_PROFILE_A2DP_SINK)
+		switch (t->type.codec) {
+		case A2DP_CODEC_SBC:
+			routine = io_thread_a2dp_sink_sbc;
+			break;
 #if ENABLE_MPEG
-			case A2DP_CODEC_MPEG12:
-				break;
+		case A2DP_CODEC_MPEG12:
+			break;
 #endif
 #if ENABLE_AAC
-			case A2DP_CODEC_MPEG24:
-				routine = io_thread_a2dp_sink_aac;
-				break;
+		case A2DP_CODEC_MPEG24:
+			routine = io_thread_a2dp_sink_aac;
+			break;
 #endif
-			default:
-				warn("Codec not supported: %u", t->type.codec);
-			}
-		break;
-	case BA_TRANSPORT_PROFILE_HFP_HF:
-	case BA_TRANSPORT_PROFILE_HFP_AG:
-	case BA_TRANSPORT_PROFILE_HSP_HS:
-	case BA_TRANSPORT_PROFILE_HSP_AG:
-		routine = io_thread_sco;
-		break;
-	case BA_TRANSPORT_PROFILE_RFCOMM:
-		routine = rfcomm_thread;
-		break;
-	}
+		default:
+			warn("Codec not supported: %u", t->type.codec);
+		}
 
 	if (routine == NULL)
 		return -1;
@@ -222,7 +213,8 @@ struct ba_transport *transport_new_rfcomm(
 	gchar *dbus_path_sco = NULL;
 	struct ba_transport *t, *t_sco;
 
-	struct ba_transport_type ttype = { .profile = BA_TRANSPORT_PROFILE_RFCOMM };
+	struct ba_transport_type ttype = {
+		.profile = type.profile | BA_TRANSPORT_PROFILE_RFCOMM };
 	if ((t = transport_new(device, ttype, dbus_owner, dbus_path)) == NULL)
 		goto fail;
 
@@ -319,26 +311,13 @@ void ba_transport_free(struct ba_transport *t) {
 
 	unsigned int pcm_type = BA_PCM_TYPE_NULL;
 
-	/* free type-specific resources */
-	switch (t->type.profile) {
-	case BA_TRANSPORT_PROFILE_A2DP_SOURCE:
-	case BA_TRANSPORT_PROFILE_A2DP_SINK:
-		pcm_type = BA_PCM_TYPE_A2DP | (t->type.profile == BA_TRANSPORT_PROFILE_A2DP_SOURCE ?
-				BA_PCM_STREAM_PLAYBACK : BA_PCM_STREAM_CAPTURE);
-		transport_release_pcm(&t->a2dp.pcm);
-		pthread_mutex_destroy(&t->a2dp.drained_mtx);
-		pthread_cond_destroy(&t->a2dp.drained);
-		free(t->a2dp.cconfig);
-		break;
-	case BA_TRANSPORT_PROFILE_RFCOMM:
+	/* free profile-specific resources */
+	if (t->type.profile & BA_TRANSPORT_PROFILE_RFCOMM) {
 		memset(&t->d->battery, 0, sizeof(t->d->battery));
 		memset(&t->d->xapl, 0, sizeof(t->d->xapl));
 		ba_transport_free(t->rfcomm.sco);
-		break;
-	case BA_TRANSPORT_PROFILE_HFP_HF:
-	case BA_TRANSPORT_PROFILE_HFP_AG:
-	case BA_TRANSPORT_PROFILE_HSP_HS:
-	case BA_TRANSPORT_PROFILE_HSP_AG:
+	}
+	else if (t->type.profile & BA_TRANSPORT_PROFILE_MASK_SCO) {
 		pcm_type = BA_PCM_TYPE_SCO | BA_PCM_STREAM_PLAYBACK | BA_PCM_STREAM_CAPTURE;
 		pthread_mutex_destroy(&t->sco.spk_drained_mtx);
 		pthread_cond_destroy(&t->sco.spk_drained);
@@ -346,7 +325,14 @@ void ba_transport_free(struct ba_transport *t) {
 		transport_release_pcm(&t->sco.mic_pcm);
 		if (t->sco.rfcomm != NULL)
 			t->sco.rfcomm->rfcomm.sco = NULL;
-		break;
+	}
+	else if (t->type.profile & BA_TRANSPORT_PROFILE_MASK_A2DP) {
+		pcm_type = BA_PCM_TYPE_A2DP | (t->type.profile == BA_TRANSPORT_PROFILE_A2DP_SOURCE ?
+				BA_PCM_STREAM_PLAYBACK : BA_PCM_STREAM_CAPTURE);
+		transport_release_pcm(&t->a2dp.pcm);
+		pthread_mutex_destroy(&t->a2dp.drained_mtx);
+		pthread_cond_destroy(&t->a2dp.drained);
+		free(t->a2dp.cconfig);
 	}
 
 	/* detach transport from the device */
@@ -433,7 +419,7 @@ unsigned int transport_get_channels(const struct ba_transport *t) {
 #endif
 		}
 
-	if (t->type.profile & BA_TRANSPORT_PROFILE_MASK_SCO)
+	if (IS_BA_TRANSPORT_PROFILE_SCO(t->type.profile))
 		return 1;
 
 	/* the number of channels is unspecified */
@@ -538,7 +524,7 @@ unsigned int transport_get_sampling(const struct ba_transport *t) {
 #endif
 		}
 
-	if (t->type.profile & BA_TRANSPORT_PROFILE_MASK_SCO)
+	if (IS_BA_TRANSPORT_PROFILE_SCO(t->type.profile))
 		switch (t->type.codec) {
 		case HFP_CODEC_UNDEFINED:
 			break;

src/ba-transport.h
@@ -36,14 +36,16 @@
 #define BA_TRANSPORT_PROFILE_HSP_AG      (2 << 4)
 #define BA_TRANSPORT_PROFILE_RFCOMM      (1 << 6)
 
-#define BA_TRANSPORT_PROFILE_MASK_A2DP (\
-		BA_TRANSPORT_PROFILE_A2DP_SOURCE | BA_TRANSPORT_PROFILE_A2DP_SINK)
-#define BA_TRANSPORT_PROFILE_MASK_HFP (\
-		BA_TRANSPORT_PROFILE_HFP_HF | BA_TRANSPORT_PROFILE_HFP_AG)
-#define BA_TRANSPORT_PROFILE_MASK_HSP (\
-		BA_TRANSPORT_PROFILE_HSP_HS | BA_TRANSPORT_PROFILE_HSP_AG)
-#define BA_TRANSPORT_PROFILE_MASK_SCO (\
-		BA_TRANSPORT_PROFILE_MASK_HFP | BA_TRANSPORT_PROFILE_MASK_HSP)
+#define BA_TRANSPORT_PROFILE_MASK_A2DP \
+	(BA_TRANSPORT_PROFILE_A2DP_SOURCE | BA_TRANSPORT_PROFILE_A2DP_SINK)
+#define BA_TRANSPORT_PROFILE_MASK_HFP \
+	(BA_TRANSPORT_PROFILE_HFP_HF | BA_TRANSPORT_PROFILE_HFP_AG)
+#define BA_TRANSPORT_PROFILE_MASK_HSP \
+	(BA_TRANSPORT_PROFILE_HSP_HS | BA_TRANSPORT_PROFILE_HSP_AG)
+#define BA_TRANSPORT_PROFILE_MASK_SCO \
+	(BA_TRANSPORT_PROFILE_MASK_HFP | BA_TRANSPORT_PROFILE_MASK_HSP)
+#define IS_BA_TRANSPORT_PROFILE_SCO(p) \
+	(((p) & BA_TRANSPORT_PROFILE_MASK_SCO) == (p))
 
 struct ba_transport_type {
 	/* Selected profile and audio codec. For A2DP vendor codecs the upper byte

src/bluez.h
@@ -11,7 +11,7 @@
 #ifndef BLUEALSA_BLUEZ_H_
 #define BLUEALSA_BLUEZ_H_
 
-/* List of Bluetoth audio profiles. */
+/* List of Bluetooth audio profiles. */
 #define BLUETOOTH_UUID_A2DP_SOURCE "0000110A-0000-1000-8000-00805F9B34FB"
 #define BLUETOOTH_UUID_A2DP_SINK   "0000110B-0000-1000-8000-00805F9B34FB"
 #define BLUETOOTH_UUID_HSP_HS      "00001108-0000-1000-8000-00805F9B34FB"

src/ctl.c
@@ -93,11 +93,11 @@ static int ctl_lookup_transport(struct ba_adapter *a, const bdaddr_t *addr,
 					return 0;
 				continue;
 			case BA_PCM_TYPE_SCO:
-				if ((*t)->type.profile & BA_TRANSPORT_PROFILE_MASK_SCO)
+				if (IS_BA_TRANSPORT_PROFILE_SCO((*t)->type.profile))
 					return 0;
 				continue;
 			case BA_PCM_TYPE_RFCOMM:
-				if ((*t)->type.profile == BA_TRANSPORT_PROFILE_RFCOMM)
+				if ((*t)->type.profile & BA_TRANSPORT_PROFILE_RFCOMM)
 					return 0;
 				continue;
 			}
@@ -119,7 +119,7 @@ static struct ba_pcm *ctl_lookup_pcm(struct ba_transport *t, uint8_t type, int c
 	if (t->type.profile & BA_TRANSPORT_PROFILE_MASK_A2DP)
 		if (t->a2dp.pcm.client == client)
 			return &t->a2dp.pcm;
-	if (t->type.profile & BA_TRANSPORT_PROFILE_MASK_SCO) {
+	if (IS_BA_TRANSPORT_PROFILE_SCO(t->type.profile)) {
 		if (type & BA_PCM_STREAM_PLAYBACK)
 			if (t->sco.spk_pcm.client == client)
 				return &t->sco.spk_pcm;
@@ -150,7 +150,7 @@ static struct ba_msg_transport *ctl_transport(const struct ba_transport *t,
 		transport->delay = t->a2dp.delay;
 	}
 
-	if (t->type.profile & BA_TRANSPORT_PROFILE_MASK_SCO) {
+	if (IS_BA_TRANSPORT_PROFILE_SCO(t->type.profile)) {
 		transport->type = BA_PCM_TYPE_SCO | BA_PCM_STREAM_PLAYBACK | BA_PCM_STREAM_CAPTURE;
 		transport->ch1_muted = t->sco.spk_muted;
 		transport->ch1_volume = t->sco.spk_gain;
@@ -336,7 +336,7 @@ static void ctl_thread_cmd_pcm_open(struct ba_ctl *ctl, struct ba_request *req, 
 
 	pthread_mutex_lock(&t->mutex);
 
-	if (t->type.profile & BA_TRANSPORT_PROFILE_MASK_SCO &&
+	if (IS_BA_TRANSPORT_PROFILE_SCO(t->type.profile) &&
 			t->type.codec == HFP_CODEC_UNDEFINED) {
 		status.code = BA_STATUS_CODE_CODEC_NOT_SELECTED;
 		goto final;
@@ -552,7 +552,7 @@ static void *ctl_thread(void *arg) {
 									transport_send_signal(t, TRANSPORT_PCM_CLOSE);
 								}
 							}
-							if (t->type.profile & BA_TRANSPORT_PROFILE_MASK_SCO) {
+							if (IS_BA_TRANSPORT_PROFILE_SCO(t->type.profile)) {
 								if (t->sco.spk_pcm.client == fd) {
 									transport_release_pcm(&t->sco.spk_pcm);
 									transport_send_signal(t, TRANSPORT_PCM_CLOSE);

src/rfcomm.c
@@ -602,7 +602,7 @@ void *rfcomm_thread(void *arg) {
 				goto ioerror;
 			}
 
-			if (t->type.profile == BA_TRANSPORT_PROFILE_HFP_HF)
+			if (t->type.profile & BA_TRANSPORT_PROFILE_HFP_HF)
 				switch (conn.state) {
 				case HFP_DISCONNECTED:
 					sprintf(tmp, "%u", config.hfp.features_rfcomm_hf);
@@ -661,7 +661,7 @@ void *rfcomm_thread(void *arg) {
 							BA_PCM_TYPE_SCO | BA_PCM_STREAM_PLAYBACK | BA_PCM_STREAM_CAPTURE);
 				}
 
-			if (t->type.profile == BA_TRANSPORT_PROFILE_HFP_AG)
+			if (t->type.profile & BA_TRANSPORT_PROFILE_HFP_AG)
 				switch (conn.state) {
 				case HFP_DISCONNECTED:
 				case HFP_SLC_BRSF_SET:

src/utils.c
@@ -553,8 +553,14 @@ const char *ba_transport_type_to_string(struct ba_transport_type type) {
 		return "HSP Headset";
 	case BA_TRANSPORT_PROFILE_HSP_AG:
 		return "HSP Audio Gateway";
-	case BA_TRANSPORT_PROFILE_RFCOMM:
-		return "RFCOMM";
+	case BA_TRANSPORT_PROFILE_RFCOMM | BA_TRANSPORT_PROFILE_HFP_HF:
+		return "RFCOMM: HFP Hands-Free";
+	case BA_TRANSPORT_PROFILE_RFCOMM | BA_TRANSPORT_PROFILE_HFP_AG:
+		return "RFCOMM: HFP Audio Gateway";
+	case BA_TRANSPORT_PROFILE_RFCOMM | BA_TRANSPORT_PROFILE_HSP_HS:
+		return "RFCOMM: HSP Headset";
+	case BA_TRANSPORT_PROFILE_RFCOMM | BA_TRANSPORT_PROFILE_HSP_AG:
+		return "RFCOMM: HSP Audio Gateway";
 	}
 	return "N/A";
 }