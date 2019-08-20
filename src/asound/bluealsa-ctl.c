arkq/bluez-alsa

commit a727f5b0d257ae25c512cc1bd82a3b4bb2343adf

Expose battery level via D-Bus PCM interface
Please note, that the Battery property is not part of the public PCM
interface. Hence, it is not documented - hidden feature.

src/asound/bluealsa-ctl.c
@@ -48,6 +48,7 @@ struct ctl_elem_update {
 struct bt_dev {
 	char device_path[sizeof(((struct ba_pcm *)0)->device_path)];
 	char name[sizeof(((struct ctl_elem *)0)->name)];
+	int battery_level;
 	int mask;
 };
 
@@ -162,6 +163,27 @@ static void bluealsa_dev_fetch_name(struct bluealsa_ctl *ctl, struct bt_dev *dev
 	dbus_message_unref(rep);
 }
 
+static void bluealsa_dev_fetch_battery(struct bluealsa_ctl *ctl, struct bt_dev *dev,
+		const char *sco_pcm_path) {
+
+	DBusMessage *rep;
+	if ((rep = bluealsa_dbus_get_property(ctl->dbus_ctx.conn, ctl->dbus_ctx.ba_service,
+					sco_pcm_path, BLUEALSA_INTERFACE_PCM, "Battery", NULL)) == NULL)
+		return;
+
+	DBusMessageIter iter;
+	DBusMessageIter iter_val;
+
+	dbus_message_iter_init(rep, &iter);
+	dbus_message_iter_recurse(&iter, &iter_val);
+
+	char battery;
+	dbus_message_iter_get_basic(&iter_val, &battery);
+	dev->battery_level = battery;
+
+	dbus_message_unref(rep);
+}
+
 /**
  * Get BT device structure.
  *
@@ -193,6 +215,7 @@ static struct bt_dev *bluealsa_dev_get(struct bluealsa_ctl *ctl, const struct ba
 	sprintf(dev->name, "%.2X:%.2X:%.2X:%.2X:%.2X:%.2X",
 			pcm->addr.b[5], pcm->addr.b[4], pcm->addr.b[3],
 			pcm->addr.b[2], pcm->addr.b[1], pcm->addr.b[0]);
+	dev->battery_level = -1;
 
 	/* Sort device list by an object path, so the bluealsa_dev_get_id() will
 	 * return consistent IDs ordering in case of name duplications. */
@@ -287,8 +310,13 @@ static int bluealsa_create_elem_list(struct bluealsa_ctl *ctl) {
 		 * transport represent both streams - playback and capture. */
 		if (ctl->pcm_list[i].flags & BA_PCM_FLAG_PROFILE_A2DP)
 			count += 2;
-		if (ctl->pcm_list[i].flags & BA_PCM_FLAG_PROFILE_SCO)
+		if (ctl->pcm_list[i].flags & BA_PCM_FLAG_PROFILE_SCO) {
 			count += 4;
+			/* It is possible, that BT device battery level will be exposed via TTY
+			 * interface, so in order to account for a special "battery" element we
+			 * have to increment our element counter by one. */
+			count += 1;
+		}
 	}
 
 	struct ctl_elem *elem_list = ctl->elem_list;
@@ -308,7 +336,6 @@ static int bluealsa_create_elem_list(struct bluealsa_ctl *ctl) {
 
 		struct ba_pcm *pcm = &ctl->pcm_list[i];
 		struct bt_dev *dev = bluealsa_dev_get(ctl, pcm);
-		dev->mask = 1;
 
 		if (pcm->flags & BA_PCM_FLAG_PROFILE_A2DP) {
 
@@ -357,6 +384,19 @@ static int bluealsa_create_elem_list(struct bluealsa_ctl *ctl) {
 			elem_list[count].playback = false;
 			bluealsa_elem_set_name(&elem_list[count], dev->name, -1);
 			count++;
+
+			if (ctl->battery) {
+				/* Add special "battery" elements. */
+				bluealsa_dev_fetch_battery(ctl, dev, pcm->pcm_path);
+				if (dev->battery_level != -1) {
+					elem_list[count].type = CTL_ELEM_TYPE_BATTERY;
+					elem_list[count].dev = dev;
+					elem_list[count].pcm = pcm;
+					elem_list[count].playback = true;
+					bluealsa_elem_set_name(&elem_list[count], dev->name, -1);
+					count++;
+				}
+			}
 
 		}
 
@@ -517,9 +557,7 @@ static int bluealsa_read_integer(snd_ctl_ext_t *ext, snd_ctl_ext_key_t key, long
 
 	switch (elem->type) {
 	case CTL_ELEM_TYPE_BATTERY:
-#if 0
-		value[0] = device->battery_level;
-#endif
+		value[0] = elem->dev->battery_level;
 		break;
 	case CTL_ELEM_TYPE_SWITCH:
 		if (pcm->flags & BA_PCM_FLAG_PROFILE_A2DP) {
@@ -684,6 +722,12 @@ static dbus_bool_t bluealsa_dbus_msg_update_dev(const char *key,
 		*stpncpy(dev->name, alias, sizeof(dev->name) - 1) = '\0';
 		dev->mask = SND_CTL_EVENT_MASK_ADD;
 	}
+	if (strcmp(key, "Battery") == 0) {
+		char battery_level;
+		dbus_message_iter_get_basic(variant, &battery_level);
+		dev->mask = dev->battery_level == -1 ? SND_CTL_EVENT_MASK_ADD : SND_CTL_EVENT_MASK_VALUE;
+		dev->battery_level = battery_level;
+	}
 
 	return TRUE;
 }
@@ -728,8 +772,18 @@ static DBusHandlerResult bluealsa_dbus_msg_filter(DBusConnection *conn,
 			for (i = 0; i < ctl->elem_list_size; i++) {
 				struct ctl_elem *elem = &ctl->elem_list[i];
 				if (strcmp(elem->pcm->pcm_path, path) == 0) {
-					bluealsa_dbus_message_iter_get_pcm_props(&iter, NULL, elem->pcm);
-					bluealsa_event_elem_updated(ctl, elem->name);
+					if (elem->type == CTL_ELEM_TYPE_BATTERY) {
+						bluealsa_dbus_message_iter_dict(&iter, NULL,
+								bluealsa_dbus_msg_update_dev, elem->dev);
+						if (elem->dev->mask & SND_CTL_EVENT_MASK_ADD)
+							goto remove_add;
+						if (elem->dev->mask & SND_CTL_EVENT_MASK_VALUE)
+							bluealsa_event_elem_updated(ctl, ctl->elem_list[i].name);
+					}
+					else {
+						bluealsa_dbus_message_iter_get_pcm_props(&iter, NULL, elem->pcm);
+						bluealsa_event_elem_updated(ctl, elem->name);
+					}
 				}
 			}
 

src/bluealsa-dbus.c
@@ -68,6 +68,10 @@ static GVariant *ba_variant_new_volume(const struct ba_transport *t) {
 	return g_variant_new_uint16(ba_transport_get_volume_packed(t));
 }
 
+static GVariant *ba_variant_new_battery(const struct ba_transport *t) {
+	return g_variant_new_byte(t->d->battery_level);
+}
+
 static void bluealsa_manager_get_pcms(GDBusMethodInvocation *inv, void *userdata) {
 	(void)userdata;
 
@@ -376,6 +380,8 @@ static GVariant *bluealsa_pcm_get_property(GDBusConnection *conn,
 		return ba_variant_new_delay(t);
 	if (strcmp(property, "Volume") == 0)
 		return ba_variant_new_volume(t);
+	if (strcmp(property, "Battery") == 0)
+		return ba_variant_new_battery(t);
 
 	*error = g_error_new(G_DBUS_ERROR, G_DBUS_ERROR_NOT_SUPPORTED,
 			"Property not supported '%s'", property);
@@ -492,6 +498,8 @@ void bluealsa_dbus_transport_update(struct ba_transport *t, unsigned int mask) {
 		g_variant_builder_add(&props, "{sv}", "Delay", ba_variant_new_delay(t));
 	if (mask & BA_DBUS_TRANSPORT_UPDATE_VOLUME)
 		g_variant_builder_add(&props, "{sv}", "Volume", ba_variant_new_volume(t));
+	if (mask & BA_DBUS_TRANSPORT_UPDATE_BATTERY)
+		g_variant_builder_add(&props, "{sv}", "Battery", ba_variant_new_battery(t));
 
 	g_dbus_connection_emit_signal(config.dbus, NULL, t->ba_dbus_path,
 			"org.freedesktop.DBus.Properties", "PropertiesChanged",

src/bluealsa-dbus.h
@@ -25,6 +25,7 @@
 #define BA_DBUS_TRANSPORT_UPDATE_CODEC    (1 << 2)
 #define BA_DBUS_TRANSPORT_UPDATE_DELAY    (1 << 3)
 #define BA_DBUS_TRANSPORT_UPDATE_VOLUME   (1 << 4)
+#define BA_DBUS_TRANSPORT_UPDATE_BATTERY  (1 << 5)
 
 int bluealsa_dbus_manager_register(GError **error);
 

src/bluealsa-iface.c
@@ -130,6 +130,10 @@ static const GDBusPropertyInfo bluealsa_iface_pcm_Volume = {
 	NULL
 };
 
+static const GDBusPropertyInfo bluealsa_iface_pcm_Battery = {
+	-1, "Battery", "y", G_DBUS_PROPERTY_INFO_FLAGS_READABLE, NULL
+};
+
 static const GDBusPropertyInfo *bluealsa_iface_pcm_properties[] = {
 	&bluealsa_iface_pcm_Device,
 	&bluealsa_iface_pcm_Modes,
@@ -138,6 +142,7 @@ static const GDBusPropertyInfo *bluealsa_iface_pcm_properties[] = {
 	&bluealsa_iface_pcm_Codec,
 	&bluealsa_iface_pcm_Delay,
 	&bluealsa_iface_pcm_Volume,
+	&bluealsa_iface_pcm_Battery,
 	NULL,
 };
 

src/rfcomm.c
@@ -213,6 +213,7 @@ static int rfcomm_handler_cind_resp_get_cb(struct rfcomm_conn *c, const struct b
 		if (c->hfp_ind_map[i] == HFP_IND_BATTCHG) {
 			d->battery_level = atoi(tmp) * 100 / 5;
 			bluealsa_ctl_send_event(d->a->ctl, BA_EVENT_BATTERY_CHANGED, &d->addr, 0);
+			bluealsa_dbus_transport_update(t->rfcomm.sco, BA_DBUS_TRANSPORT_UPDATE_BATTERY);
 		}
 		if ((tmp = strchr(tmp, ',')) == NULL)
 			break;
@@ -259,6 +260,7 @@ static int rfcomm_handler_ciev_resp_cb(struct rfcomm_conn *c, const struct bt_at
 		case HFP_IND_BATTCHG:
 			d->battery_level = value * 100 / 5;
 			bluealsa_ctl_send_event(d->a->ctl, BA_EVENT_BATTERY_CHANGED, &d->addr, 0);
+			bluealsa_dbus_transport_update(t->rfcomm.sco, BA_DBUS_TRANSPORT_UPDATE_BATTERY);
 			break;
 		default:
 			break;
@@ -464,6 +466,7 @@ static int rfcomm_handler_iphoneaccev_set_cb(struct rfcomm_conn *c, const struct
 			if (ptr != NULL) {
 				d->battery_level = atoi(strsep(&ptr, ",")) * 100 / 9;
 				bluealsa_ctl_send_event(d->a->ctl, BA_EVENT_BATTERY_CHANGED, &d->addr, 0);
+				bluealsa_dbus_transport_update(t->rfcomm.sco, BA_DBUS_TRANSPORT_UPDATE_BATTERY);
 			}
 			break;
 		case '2':

commit eb3d785a5fa20be37184312b9be34ecef3a7eef5

Fix battery level indicator broken since a727f5b

src/asound/bluealsa-ctl.c
@@ -960,6 +960,8 @@ SND_CTL_PLUGIN_DEFINE_FUNC(bluealsa) {
 	if ((ctl = calloc(1, sizeof(*ctl))) == NULL)
 		return -ENOMEM;
 
+	ctl->battery = strcmp(battery, "yes") == 0;
+
 	dbus_threads_init_default();
 
 	if (!bluealsa_dbus_connection_ctx_init(&ctl->dbus_ctx, service, &err)) {
@@ -993,7 +995,6 @@ SND_CTL_PLUGIN_DEFINE_FUNC(bluealsa) {
 	strncpy(ctl->ext.name, "BlueALSA", sizeof(ctl->ext.name) - 1);
 	strncpy(ctl->ext.longname, "Bluetooth Audio Hub Controller", sizeof(ctl->ext.longname) - 1);
 	strncpy(ctl->ext.mixername, "BlueALSA Plugin", sizeof(ctl->ext.mixername) - 1);
-	ctl->battery = strcmp(battery, "yes") == 0;
 
 	ctl->ext.callback = &bluealsa_snd_ctl_ext_callback;
 	ctl->ext.private_data = ctl;