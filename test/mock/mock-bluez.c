6c75b5519d42810921d40517c38dede41e280065
Arkadiusz Bokowy <arkadiusz.bokowy@gmail.com>
2024-04-24 20:05:57 +0200

    Define BlueZ mock server interfaces with XML file

--- test/mock/mock-bluez.c
+++ test/mock/mock-bluez.c
@@ -1,6 +1,6 @@
 /*
  * mock-bluez.c
- * Copyright (c) 2016-2023 Arkadiusz Bokowy
+ * Copyright (c) 2016-2024 Arkadiusz Bokowy
  *
  * This file is a part of bluez-alsa.
  *
@@ -13,132 +13,19 @@
 #include <stdio.h>
 #include <string.h>
 
-#include <bluetooth/bluetooth.h>
-
 #include <gio/gio.h>
+#include <glib-object.h>
 #include <glib.h>
 
 #include "ba-config.h"
 #include "bluez-iface.h"
-#include "utils.h"
+#include "mock-bluez-iface.h"
 #include "shared/defs.h"
 
-/**
- * Bluetooth device name mappings in form of "MAC:name". */
+/* Bluetooth device name mappings in form of "MAC:name". */
 static const char * devices[8] = { NULL };
-
-static GDBusPropertyInfo bluez_iface_device_Adapter = {
-	-1, "Adapter", "o", G_DBUS_PROPERTY_INFO_FLAGS_READABLE, NULL
-};
-
-static GDBusPropertyInfo bluez_iface_device_Alias = {
-	-1, "Alias", "s", G_DBUS_PROPERTY_INFO_FLAGS_READABLE, NULL
-};
-
-static GDBusPropertyInfo bluez_iface_device_Class = {
-	-1, "Class", "s", G_DBUS_PROPERTY_INFO_FLAGS_READABLE, NULL
-};
-
-static GDBusPropertyInfo bluez_iface_device_Icon = {
-	-1, "Icon", "s", G_DBUS_PROPERTY_INFO_FLAGS_READABLE, NULL
-};
-
-static GDBusPropertyInfo bluez_iface_device_Connected = {
-	-1, "Connected", "s", G_DBUS_PROPERTY_INFO_FLAGS_READABLE, NULL
-};
-
-static GDBusPropertyInfo *bluez_iface_device_properties[] = {
-	&bluez_iface_device_Adapter,
-	&bluez_iface_device_Alias,
-	&bluez_iface_device_Class,
-	&bluez_iface_device_Icon,
-	&bluez_iface_device_Connected,
-	NULL,
-};
-
-static GDBusInterfaceInfo bluez_iface_device = {
-	-1, BLUEZ_IFACE_DEVICE,
-	NULL,
-	NULL,
-	bluez_iface_device_properties,
-	NULL,
-};
-
-static GDBusMethodInfo bluez_iface_media_transport_Release = {
-	-1, "Release", NULL, NULL, NULL
-};
-
-static GDBusMethodInfo *bluez_iface_media_transport_methods[] = {
-	&bluez_iface_media_transport_Release,
-	NULL,
-};
-
-static GDBusInterfaceInfo bluez_iface_media_transport = {
-	-1, BLUEZ_IFACE_MEDIA_TRANSPORT,
-	bluez_iface_media_transport_methods,
-	NULL,
-	NULL,
-	NULL,
-};
-
-static GVariant *bluez_device_get_property(GDBusConnection *conn,
-		const char *sender, const char *path, const char *iface,
-		const char *property, GError **error, void *userdata) {
-	(void)conn;
-	(void)sender;
-	(void)iface;
-	(void)userdata;
-
-	bdaddr_t addr;
-	char addrstr[18];
-	ba2str(g_dbus_bluez_object_path_to_bdaddr(path, &addr), addrstr);
-
-	if (strcmp(property, "Adapter") == 0)
-		return g_variant_new_object_path(MOCK_BLUEZ_ADAPTER_PATH);
-	if (strcmp(property, "Class") == 0)
-		return g_variant_new_uint32(0x240404);
-	if (strcmp(property, "Icon") == 0)
-		return g_variant_new_string("audio-card");
-	if (strcmp(property, "Connected") == 0)
-		return g_variant_new_boolean(TRUE);
-
-	if (strcmp(property, "Alias") == 0) {
-
-		for (size_t i = 0; i < ARRAYSIZE(devices); i++)
-			if (devices[i] != NULL &&
-					strncmp(devices[i], addrstr, sizeof(addrstr) - 1) == 0)
-				return g_variant_new_string(&devices[i][sizeof(addrstr)]);
-
-		if (error != NULL)
-			*error = g_error_new(G_DBUS_ERROR, G_DBUS_ERROR_UNKNOWN_PROPERTY,
-					"Device alias/name not available");
-		return NULL;
-	}
-
-	g_assert_not_reached();
-	return NULL;
-}
-
-static const GDBusInterfaceVTable bluez_device_vtable = {
-	.get_property = bluez_device_get_property,
-};
-
-static void bluez_media_transport_method_call(G_GNUC_UNUSED GDBusConnection *conn,
-		G_GNUC_UNUSED const char *sender, G_GNUC_UNUSED const char *path,
-		G_GNUC_UNUSED const char *interface, const char *method, G_GNUC_UNUSED GVariant *params,
-		GDBusMethodInvocation *invocation, G_GNUC_UNUSED void *userdata) {
-
-	if (strcmp(method, "Release") == 0) {
-		g_dbus_method_invocation_return_value(invocation, NULL);
-		return;
-	}
-
-	g_assert_not_reached();
-}
-
-static const GDBusInterfaceVTable bluez_media_transport_vtable = {
-	.method_call = bluez_media_transport_method_call,
-};
+/* Global BlueZ mock server object manager. */
+static GDBusObjectManagerServer *server = NULL;
 
 int mock_bluez_device_name_mapping_add(const char *mapping) {
 	for (size_t i = 0; i < ARRAYSIZE(devices); i++)
@@ -149,19 +36,67 @@ int mock_bluez_device_name_mapping_add(const char *mapping) {
 	return -1;
 }
 
+static void mock_bluez_adapter_add(const char *path, const char *address) {
+
+	g_autoptr(MockBluezAdapter1) adapter = mock_bluez_adapter1_skeleton_new();
+	mock_bluez_adapter1_set_address(adapter, address);
+
+	g_autoptr(GDBusObjectSkeleton) skeleton = g_dbus_object_skeleton_new(path);
+	g_dbus_object_skeleton_add_interface(skeleton, G_DBUS_INTERFACE_SKELETON(adapter));
+	g_dbus_object_manager_server_export(server, skeleton);
+
+}
+
+static void mock_bluez_device_add(const char *path, const char *adapter, const char *address) {
+
+	g_autoptr(MockBluezDevice1) device = mock_bluez_device1_skeleton_new();
+	mock_bluez_device1_set_adapter(device, adapter);
+	mock_bluez_device1_set_alias(device, address);
+	mock_bluez_device1_set_icon(device, "audio-card");
+
+	for (size_t i = 0; i < ARRAYSIZE(devices); i++)
+		if (devices[i] != NULL &&
+				strncmp(devices[i], address, strlen(address)) == 0)
+			mock_bluez_device1_set_alias(device, &devices[i][strlen(address) + 1]);
+
+	g_autoptr(GDBusObjectSkeleton) skeleton = g_dbus_object_skeleton_new(path);
+	g_dbus_object_skeleton_add_interface(skeleton, G_DBUS_INTERFACE_SKELETON(device));
+	g_dbus_object_manager_server_export(server, skeleton);
+
+}
+
+static gboolean mock_bluez_media_transport_release_handler(MockBluezMediaTransport1 *transport,
+		GDBusMethodInvocation *invocation, G_GNUC_UNUSED void *userdata) {
+	mock_bluez_media_transport1_complete_release(transport, invocation);
+	return TRUE;
+}
+
+static void mock_bluez_media_transport_add(const char *path, const char *device) {
+
+	g_autoptr(MockBluezMediaTransport1) transport = mock_bluez_media_transport1_skeleton_new();
+	mock_bluez_media_transport1_set_device(transport, device);
+
+	g_signal_connect(transport, "handle-release",
+			G_CALLBACK(mock_bluez_media_transport_release_handler), NULL);
+
+	g_autoptr(GDBusObjectSkeleton) skeleton = g_dbus_object_skeleton_new(path);
+	g_dbus_object_skeleton_add_interface(skeleton, G_DBUS_INTERFACE_SKELETON(transport));
+	g_dbus_object_manager_server_export(server, skeleton);
+
+}
+
 static void mock_bluez_dbus_name_acquired(GDBusConnection *conn,
 		G_GNUC_UNUSED const char *name, void *userdata) {
 
-	g_dbus_connection_register_object(conn, MOCK_BLUEZ_DEVICE_PATH_1,
-			&bluez_iface_device, &bluez_device_vtable, NULL, NULL, NULL);
-	g_dbus_connection_register_object(conn, MOCK_BLUEZ_DEVICE_PATH_2,
-			&bluez_iface_device, &bluez_device_vtable, NULL, NULL, NULL);
+	server = g_dbus_object_manager_server_new("/");
 
-	g_dbus_connection_register_object(conn, MOCK_BLUEZ_MEDIA_TRANSPORT_PATH_1,
-			&bluez_iface_media_transport, &bluez_media_transport_vtable, NULL, NULL, NULL);
-	g_dbus_connection_register_object(conn, MOCK_BLUEZ_MEDIA_TRANSPORT_PATH_2,
-			&bluez_iface_media_transport, &bluez_media_transport_vtable, NULL, NULL, NULL);
+	mock_bluez_adapter_add(MOCK_BLUEZ_ADAPTER_PATH, "00:00:11:11:22:22");
+	mock_bluez_device_add(MOCK_BLUEZ_DEVICE_PATH_1, MOCK_BLUEZ_ADAPTER_PATH, MOCK_DEVICE_1);
+	mock_bluez_media_transport_add(MOCK_BLUEZ_MEDIA_TRANSPORT_PATH_1, MOCK_BLUEZ_DEVICE_PATH_1);
+	mock_bluez_device_add(MOCK_BLUEZ_DEVICE_PATH_2, MOCK_BLUEZ_ADAPTER_PATH, MOCK_DEVICE_2);
+	mock_bluez_media_transport_add(MOCK_BLUEZ_MEDIA_TRANSPORT_PATH_2, MOCK_BLUEZ_DEVICE_PATH_2);
 
+	g_dbus_object_manager_server_set_connection(server, conn);
 	mock_sem_signal(userdata);
 
 }
--- utils/aplay/dbus.c
+++ utils/aplay/dbus.c
@@ -1,6 +1,6 @@
 /*
  * BlueALSA - dbus.c
- * Copyright (c) 2016-2021 Arkadiusz Bokowy
+ * Copyright (c) 2016-2024 Arkadiusz Bokowy
  *
  * This file is a part of bluez-alsa.
  *
@@ -15,11 +15,6 @@
 
 static bool dbus_message_iter_get_basic_boolean(DBusMessageIter *iter) {
 	dbus_bool_t tmp;
-	return dbus_message_iter_get_basic(iter, &tmp), tmp;
-}
-
-static unsigned int dbus_message_iter_get_basic_integer(DBusMessageIter *iter) {
-	dbus_uint32_t tmp;
 	return dbus_message_iter_get_basic(iter, &tmp), tmp;
 }
 
@@ -107,14 +102,8 @@ int dbus_bluez_get_device(DBusConnection *conn, const char *path,
 			str2ba(dbus_message_iter_get_basic_string(&iter_entry_val), &dev->bt_addr);
 		else if (strcmp(key, "Alias") == 0)
 			strncpy(dev->name, dbus_message_iter_get_basic_string(&iter_entry_val), sizeof(dev->name) - 1);
-		else if (strcmp(key, "Class") == 0)
-			dev->class_ = dbus_message_iter_get_basic_integer(&iter_entry_val);
 		else if (strcmp(key, "Icon") == 0)
 			strncpy(dev->icon, dbus_message_iter_get_basic_string(&iter_entry_val), sizeof(dev->icon) - 1);
-		else if (strcmp(key, "Blocked") == 0)
-			dev->blocked = dbus_message_iter_get_basic_boolean(&iter_entry_val);
-		else if (strcmp(key, "Connected") == 0)
-			dev->connected = dbus_message_iter_get_basic_boolean(&iter_entry_val);
 		else if (strcmp(key, "Paired") == 0)
 			dev->paired = dbus_message_iter_get_basic_boolean(&iter_entry_val);
 		else if (strcmp(key, "Trusted") == 0)
