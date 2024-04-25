arkq/bluez-alsa

commit a8b10fef4f75ae028bd36eace28dc1dae087a4a6

Keep A2DP configuration check in codec files
In order to simplify error handling, the check function stops on first
error. Now, the D-Bus API for codec selection returns human readable
error message describing the cause of error.

src/a2dp-faststream.c
@@ -289,6 +289,40 @@ static int a2dp_faststream_configuration_select(
 	return 0;
 }
 
+static int a2dp_faststream_configuration_check(
+		const struct a2dp_codec *codec,
+		const void *configuration) {
+
+	const a2dp_faststream_t *conf = configuration;
+	a2dp_faststream_t conf_v = *conf;
+
+	/* validate configuration against BlueALSA capabilities */
+	if (a2dp_filter_capabilities(codec, &codec->capabilities,
+				&conf_v, sizeof(conf_v)) != 0)
+		return A2DP_CHECK_ERR_SIZE;
+
+	switch (conf_v.direction) {
+	case FASTSTREAM_DIRECTION_MUSIC:
+	case FASTSTREAM_DIRECTION_VOICE:
+		break;
+	default:
+		debug("FastStream: Invalid direction: %#x", conf->direction);
+		return A2DP_CHECK_ERR_DIRECTIONS;
+	}
+
+	if (a2dp_sampling_lookup(a2dp_faststream_samplings_voice, conf_v.frequency_voice) == NULL) {
+		debug("FastStream: Invalid voice sampling frequency: %#x", conf->frequency_voice);
+		return A2DP_CHECK_ERR_SAMPLING_VOICE;
+	}
+
+	if (a2dp_sampling_lookup(a2dp_faststream_samplings_music, conf_v.frequency_music) == NULL) {
+		debug("FastStream: Invalid music sampling frequency: %#x", conf->frequency_music);
+		return A2DP_CHECK_ERR_SAMPLING_MUSIC;
+	}
+
+	return A2DP_CHECK_OK;
+}
+
 static int a2dp_faststream_transport_init(struct ba_transport *t) {
 
 	if (t->a2dp.configuration.faststream.direction & FASTSTREAM_DIRECTION_MUSIC) {
@@ -356,10 +390,9 @@ struct a2dp_codec a2dp_faststream_source = {
 			FASTSTREAM_SAMPLING_FREQ_VOICE_16000,
 	},
 	.capabilities_size = sizeof(a2dp_faststream_t),
-	.samplings[0] = a2dp_faststream_samplings_music,
-	.samplings[1] = a2dp_faststream_samplings_voice,
 	.init = a2dp_faststream_source_init,
 	.configuration_select = a2dp_faststream_configuration_select,
+	.configuration_check = a2dp_faststream_configuration_check,
 	.transport_init = a2dp_faststream_transport_init,
 	.transport_start = a2dp_faststream_source_transport_start,
 };
@@ -392,9 +425,8 @@ struct a2dp_codec a2dp_faststream_sink = {
 			FASTSTREAM_SAMPLING_FREQ_VOICE_16000,
 	},
 	.capabilities_size = sizeof(a2dp_faststream_t),
-	.samplings[0] = a2dp_faststream_samplings_music,
-	.samplings[1] = a2dp_faststream_samplings_voice,
 	.configuration_select = a2dp_faststream_configuration_select,
+	.configuration_check = a2dp_faststream_configuration_check,
 	.transport_init = a2dp_faststream_transport_init,
 	.transport_start = a2dp_faststream_sink_transport_start,
 };

test/test-a2dp.c
@@ -160,8 +160,17 @@ CK_START_TEST(test_a2dp_check_configuration) {
 	};
 
 	ck_assert_int_eq(a2dp_check_configuration(&a2dp_sbc_source,
-				&cfg_invalid, sizeof(cfg_invalid)),
-			A2DP_CHECK_ERR_SAMPLING | A2DP_CHECK_ERR_CHANNELS | A2DP_CHECK_ERR_SBC_SUB_BANDS);
+				&cfg_invalid, sizeof(cfg_invalid)), A2DP_CHECK_ERR_SAMPLING);
+
+#if ENABLE_AAC
+	a2dp_aac_t cfg_aac_invalid = {
+		/* FDK-AAC encoder does not support AAC Long Term Prediction */
+		.object_type = AAC_OBJECT_TYPE_MPEG4_AAC_LTP,
+		AAC_INIT_FREQUENCY(AAC_SAMPLING_FREQ_44100)
+		.channels = AAC_CHANNELS_1 };
+	ck_assert_int_eq(a2dp_check_configuration(&a2dp_aac_source,
+			&cfg_aac_invalid, sizeof(cfg_aac_invalid)), A2DP_CHECK_ERR_OBJECT_TYPE);
+#endif
 
 } CK_END_TEST
 

src/a2dp-aac.c
@@ -552,6 +552,43 @@ static int a2dp_aac_configuration_select(
 	return 0;
 }
 
+static int a2dp_aac_configuration_check(
+		const struct a2dp_codec *codec,
+		const void *configuration) {
+
+	const a2dp_aac_t *conf = configuration;
+	a2dp_aac_t conf_v = *conf;
+
+	/* validate configuration against BlueALSA capabilities */
+	if (a2dp_filter_capabilities(codec, &codec->capabilities,
+				&conf_v, sizeof(conf_v)) != 0)
+		return A2DP_CHECK_ERR_SIZE;
+
+	switch (conf_v.object_type) {
+	case AAC_OBJECT_TYPE_MPEG2_AAC_LC:
+	case AAC_OBJECT_TYPE_MPEG4_AAC_LC:
+	case AAC_OBJECT_TYPE_MPEG4_AAC_LTP:
+	case AAC_OBJECT_TYPE_MPEG4_AAC_SCA:
+		break;
+	default:
+		debug("AAC: Invalid object type: %#x", conf->object_type);
+		return A2DP_CHECK_ERR_OBJECT_TYPE;
+	}
+
+	const uint16_t conf_frequency = AAC_GET_FREQUENCY(conf_v);
+	if (a2dp_sampling_lookup(a2dp_aac_samplings, conf_frequency) == NULL) {
+		debug("AAC: Invalid sampling frequency: %#x", AAC_GET_FREQUENCY(*conf));
+		return A2DP_CHECK_ERR_SAMPLING;
+	}
+
+	if (a2dp_channel_mode_lookup(a2dp_aac_channels, conf_v.channels) == NULL) {
+		debug("AAC: Invalid channel mode: %#x", conf->channels);
+		return A2DP_CHECK_ERR_CHANNEL_MODE;
+	}
+
+	return A2DP_CHECK_OK;
+}
+
 static int a2dp_aac_transport_init(struct ba_transport *t) {
 
 	const struct a2dp_channel_mode *chm;
@@ -630,11 +667,10 @@ struct a2dp_codec a2dp_aac_source = {
 		AAC_INIT_BITRATE(320000)
 	},
 	.capabilities_size = sizeof(a2dp_aac_t),
-	.channels[0] = a2dp_aac_channels,
-	.samplings[0] = a2dp_aac_samplings,
 	.init = a2dp_aac_source_init,
 	.capabilities_filter = a2dp_aac_capabilities_filter,
 	.configuration_select = a2dp_aac_configuration_select,
+	.configuration_check = a2dp_aac_configuration_check,
 	.transport_init = a2dp_aac_transport_init,
 	.transport_start = a2dp_aac_source_transport_start,
 	.enabled = true,
@@ -691,11 +727,10 @@ struct a2dp_codec a2dp_aac_sink = {
 		AAC_INIT_BITRATE(320000)
 	},
 	.capabilities_size = sizeof(a2dp_aac_t),
-	.channels[0] = a2dp_aac_channels,
-	.samplings[0] = a2dp_aac_samplings,
 	.init = a2dp_aac_sink_init,
 	.capabilities_filter = a2dp_aac_capabilities_filter,
 	.configuration_select = a2dp_aac_configuration_select,
+	.configuration_check = a2dp_aac_configuration_check,
 	.transport_init = a2dp_aac_transport_init,
 	.transport_start = a2dp_aac_sink_transport_start,
 	.enabled = true,

src/a2dp-aptx-hd.c
@@ -313,6 +313,31 @@ static int a2dp_aptx_hd_configuration_select(
 	return 0;
 }
 
+static int a2dp_aptx_hd_configuration_check(
+		const struct a2dp_codec *codec,
+		const void *configuration) {
+
+	const a2dp_aptx_hd_t *conf = configuration;
+	a2dp_aptx_hd_t conf_v = *conf;
+
+	/* validate configuration against BlueALSA capabilities */
+	if (a2dp_filter_capabilities(codec, &codec->capabilities,
+				&conf_v, sizeof(conf_v)) != 0)
+		return A2DP_CHECK_ERR_SIZE;
+
+	if (a2dp_sampling_lookup(a2dp_aptx_hd_samplings, conf_v.aptx.frequency) == NULL) {
+		debug("apt-X HD: Invalid sampling frequency: %#x", conf->aptx.frequency);
+		return A2DP_CHECK_ERR_SAMPLING;
+	}
+
+	if (a2dp_channel_mode_lookup(a2dp_aptx_hd_channels, conf_v.aptx.channel_mode) == NULL) {
+		debug("apt-X HD: Invalid channel mode: %#x", conf->aptx.channel_mode);
+		return A2DP_CHECK_ERR_CHANNEL_MODE;
+	}
+
+	return A2DP_CHECK_OK;
+}
+
 static int a2dp_aptx_hd_transport_init(struct ba_transport *t) {
 
 	const struct a2dp_channel_mode *chm;
@@ -361,10 +386,9 @@ struct a2dp_codec a2dp_aptx_hd_source = {
 			APTX_SAMPLING_FREQ_48000,
 	},
 	.capabilities_size = sizeof(a2dp_aptx_hd_t),
-	.channels[0] = a2dp_aptx_hd_channels,
-	.samplings[0] = a2dp_aptx_hd_samplings,
 	.init = a2dp_aptx_hd_source_init,
 	.configuration_select = a2dp_aptx_hd_configuration_select,
+	.configuration_check = a2dp_aptx_hd_configuration_check,
 	.transport_init = a2dp_aptx_hd_transport_init,
 	.transport_start = a2dp_aptx_hd_source_transport_start,
 };
@@ -392,9 +416,8 @@ struct a2dp_codec a2dp_aptx_hd_sink = {
 			APTX_SAMPLING_FREQ_48000,
 	},
 	.capabilities_size = sizeof(a2dp_aptx_hd_t),
-	.channels[0] = a2dp_aptx_hd_channels,
-	.samplings[0] = a2dp_aptx_hd_samplings,
 	.configuration_select = a2dp_aptx_hd_configuration_select,
+	.configuration_check = a2dp_aptx_hd_configuration_check,
 	.transport_init = a2dp_aptx_hd_transport_init,
 	.transport_start = a2dp_aptx_hd_sink_transport_start,
 };

src/a2dp-aptx.c
@@ -276,6 +276,31 @@ static int a2dp_aptx_configuration_select(
 	return 0;
 }
 
+static int a2dp_aptx_configuration_check(
+		const struct a2dp_codec *codec,
+		const void *configuration) {
+
+	const a2dp_aptx_t *conf = configuration;
+	a2dp_aptx_t conf_v = *conf;
+
+	/* validate configuration against BlueALSA capabilities */
+	if (a2dp_filter_capabilities(codec, &codec->capabilities,
+				&conf_v, sizeof(conf_v)) != 0)
+		return A2DP_CHECK_ERR_SIZE;
+
+	if (a2dp_sampling_lookup(a2dp_aptx_samplings, conf_v.frequency) == NULL) {
+		debug("apt-X: Invalid sampling frequency: %#x", conf->frequency);
+		return A2DP_CHECK_ERR_SAMPLING;
+	}
+
+	if (a2dp_channel_mode_lookup(a2dp_aptx_channels, conf_v.channel_mode) == NULL) {
+		debug("apt-X: Invalid channel mode: %#x", conf->channel_mode);
+		return A2DP_CHECK_ERR_CHANNEL_MODE;
+	}
+
+	return A2DP_CHECK_OK;
+}
+
 static int a2dp_aptx_transport_init(struct ba_transport *t) {
 
 	const struct a2dp_channel_mode *chm;
@@ -324,10 +349,9 @@ struct a2dp_codec a2dp_aptx_source = {
 			APTX_SAMPLING_FREQ_48000,
 	},
 	.capabilities_size = sizeof(a2dp_aptx_t),
-	.channels[0] = a2dp_aptx_channels,
-	.samplings[0] = a2dp_aptx_samplings,
 	.init = a2dp_aptx_source_init,
 	.configuration_select = a2dp_aptx_configuration_select,
+	.configuration_check = a2dp_aptx_configuration_check,
 	.transport_init = a2dp_aptx_transport_init,
 	.transport_start = a2dp_aptx_source_transport_start,
 };
@@ -355,9 +379,8 @@ struct a2dp_codec a2dp_aptx_sink = {
 			APTX_SAMPLING_FREQ_48000,
 	},
 	.capabilities_size = sizeof(a2dp_aptx_t),
-	.channels[0] = a2dp_aptx_channels,
-	.samplings[0] = a2dp_aptx_samplings,
 	.configuration_select = a2dp_aptx_configuration_select,
+	.configuration_check = a2dp_aptx_configuration_check,
 	.transport_init = a2dp_aptx_transport_init,
 	.transport_start = a2dp_aptx_sink_transport_start,
 };

src/a2dp-lc3plus.c
@@ -590,6 +590,42 @@ static int a2dp_lc3plus_configuration_select(
 	return 0;
 }
 
+static int a2dp_lc3plus_configuration_check(
+		const struct a2dp_codec *codec,
+		const void *configuration) {
+
+	const a2dp_lc3plus_t *conf = configuration;
+	a2dp_lc3plus_t conf_v = *conf;
+
+	/* validate configuration against BlueALSA capabilities */
+	if (a2dp_filter_capabilities(codec, &codec->capabilities,
+				&conf_v, sizeof(conf_v)) != 0)
+		return A2DP_CHECK_ERR_SIZE;
+
+	switch (conf_v.frame_duration) {
+	case LC3PLUS_FRAME_DURATION_025:
+	case LC3PLUS_FRAME_DURATION_050:
+	case LC3PLUS_FRAME_DURATION_100:
+		break;
+	default:
+		debug("LC3plus: Invalid frame duration: %#x", conf->frame_duration);
+		return A2DP_CHECK_ERR_FRAME_DURATION;
+	}
+
+	if (a2dp_channel_mode_lookup(a2dp_lc3plus_channels, conf_v.channels) == NULL) {
+		debug("LC3plus: Invalid channel mode: %#x", conf->channels);
+		return A2DP_CHECK_ERR_CHANNEL_MODE;
+	}
+
+	uint16_t conf_frequency = LC3PLUS_GET_FREQUENCY(conf_v);
+	if (a2dp_sampling_lookup(a2dp_lc3plus_samplings, conf_frequency) == NULL) {
+		debug("LC3plus: Invalid sampling frequency: %#x", LC3PLUS_GET_FREQUENCY(*conf));
+		return A2DP_CHECK_ERR_SAMPLING;
+	}
+
+	return A2DP_CHECK_OK;
+}
+
 static int a2dp_lc3plus_transport_init(struct ba_transport *t) {
 
 	const struct a2dp_channel_mode *chm;
@@ -639,10 +675,9 @@ struct a2dp_codec a2dp_lc3plus_source = {
 				LC3PLUS_SAMPLING_FREQ_96000)
 	},
 	.capabilities_size = sizeof(a2dp_lc3plus_t),
-	.channels[0] = a2dp_lc3plus_channels,
-	.samplings[0] = a2dp_lc3plus_samplings,
 	.init = a2dp_lc3plus_source_init,
 	.configuration_select = a2dp_lc3plus_configuration_select,
+	.configuration_check = a2dp_lc3plus_configuration_check,
 	.transport_init = a2dp_lc3plus_transport_init,
 	.transport_start = a2dp_lc3plus_source_transport_start,
 };
@@ -669,9 +704,8 @@ struct a2dp_codec a2dp_lc3plus_sink = {
 				LC3PLUS_SAMPLING_FREQ_96000)
 	},
 	.capabilities_size = sizeof(a2dp_lc3plus_t),
-	.channels[0] = a2dp_lc3plus_channels,
-	.samplings[0] = a2dp_lc3plus_samplings,
 	.configuration_select = a2dp_lc3plus_configuration_select,
+	.configuration_check = a2dp_lc3plus_configuration_check,
 	.transport_init = a2dp_lc3plus_transport_init,
 	.transport_start = a2dp_lc3plus_sink_transport_start,
 };

src/a2dp-ldac.c
@@ -368,6 +368,31 @@ static int a2dp_ldac_configuration_select(
 	return 0;
 }
 
+static int a2dp_ldac_configuration_check(
+		const struct a2dp_codec *codec,
+		const void *configuration) {
+
+	const a2dp_ldac_t *conf = configuration;
+	a2dp_ldac_t conf_v = *conf;
+
+	/* validate configuration against BlueALSA capabilities */
+	if (a2dp_filter_capabilities(codec, &codec->capabilities,
+				&conf_v, sizeof(conf_v)) != 0)
+		return A2DP_CHECK_ERR_SIZE;
+
+	if (a2dp_sampling_lookup(a2dp_ldac_samplings, conf_v.frequency) == NULL) {
+		debug("LDAC: Invalid sampling frequency: %#x", conf->frequency);
+		return A2DP_CHECK_ERR_SAMPLING;
+	}
+
+	if (a2dp_channel_mode_lookup(a2dp_ldac_channels, conf_v.channel_mode) == NULL) {
+		debug("LDAC: Invalid channel mode: %#x", conf->channel_mode);
+		return A2DP_CHECK_ERR_CHANNEL_MODE;
+	}
+
+	return A2DP_CHECK_OK;
+}
+
 static int a2dp_ldac_transport_init(struct ba_transport *t) {
 
 	const struct a2dp_channel_mode *chm;
@@ -420,10 +445,9 @@ struct a2dp_codec a2dp_ldac_source = {
 			LDAC_SAMPLING_FREQ_96000,
 	},
 	.capabilities_size = sizeof(a2dp_ldac_t),
-	.channels[0] = a2dp_ldac_channels,
-	.samplings[0] = a2dp_ldac_samplings,
 	.init = a2dp_ldac_source_init,
 	.configuration_select = a2dp_ldac_configuration_select,
+	.configuration_check = a2dp_ldac_configuration_check,
 	.transport_init = a2dp_ldac_transport_init,
 	.transport_start = a2dp_ldac_source_transport_start,
 };
@@ -453,9 +477,8 @@ struct a2dp_codec a2dp_ldac_sink = {
 			LDAC_SAMPLING_FREQ_96000,
 	},
 	.capabilities_size = sizeof(a2dp_ldac_t),
-	.channels[0] = a2dp_ldac_channels,
-	.samplings[0] = a2dp_ldac_samplings,
 	.configuration_select = a2dp_ldac_configuration_select,
+	.configuration_check = a2dp_ldac_configuration_check,
 	.transport_init = a2dp_ldac_transport_init,
 	.transport_start = a2dp_ldac_sink_transport_start,
 };

src/a2dp-mpeg.c
@@ -522,6 +522,41 @@ static int a2dp_mpeg_configuration_select(
 	return 0;
 }
 
+static int a2dp_mpeg_configuration_check(
+		const struct a2dp_codec *codec,
+		const void *configuration) {
+
+	const a2dp_mpeg_t *conf = configuration;
+	a2dp_mpeg_t conf_v = *conf;
+
+	/* validate configuration against BlueALSA capabilities */
+	if (a2dp_filter_capabilities(codec, &codec->capabilities,
+				&conf_v, sizeof(conf_v)) != 0)
+		return A2DP_CHECK_ERR_SIZE;
+
+	switch (conf_v.layer) {
+	case MPEG_LAYER_MP1:
+	case MPEG_LAYER_MP2:
+	case MPEG_LAYER_MP3:
+		break;
+	default:
+		debug("MPEG: Invalid layer: %#x", conf->layer);
+		return A2DP_CHECK_ERR_MPEG_LAYER;
+	}
+
+	if (a2dp_channel_mode_lookup(a2dp_mpeg_channels, conf_v.channel_mode) == NULL) {
+		debug("MPEG: Invalid channel mode: %#x", conf->channel_mode);
+		return A2DP_CHECK_ERR_CHANNEL_MODE;
+	}
+
+	if (a2dp_sampling_lookup(a2dp_mpeg_samplings, conf_v.frequency) == NULL) {
+		debug("MPEG: Invalid sampling frequency: %#x", conf->frequency);
+		return A2DP_CHECK_ERR_SAMPLING;
+	}
+
+	return A2DP_CHECK_OK;
+}
+
 static int a2dp_mpeg_transport_init(struct ba_transport *t) {
 
 	const struct a2dp_channel_mode *chm;
@@ -601,10 +636,9 @@ struct a2dp_codec a2dp_mpeg_source = {
 		)
 	},
 	.capabilities_size = sizeof(a2dp_mpeg_t),
-	.channels[0] = a2dp_mpeg_channels,
-	.samplings[0] = a2dp_mpeg_samplings,
 	.init = a2dp_mpeg_source_init,
 	.configuration_select = a2dp_mpeg_configuration_select,
+	.configuration_check = a2dp_mpeg_configuration_check,
 	.transport_init = a2dp_mpeg_transport_init,
 	.transport_start = a2dp_mpeg_source_transport_start,
 	/* TODO: This is an optional but covered by the A2DP spec codec,
@@ -679,9 +713,8 @@ struct a2dp_codec a2dp_mpeg_sink = {
 		)
 	},
 	.capabilities_size = sizeof(a2dp_mpeg_t),
-	.channels[0] = a2dp_mpeg_channels,
-	.samplings[0] = a2dp_mpeg_samplings,
 	.configuration_select = a2dp_mpeg_configuration_select,
+	.configuration_check = a2dp_mpeg_configuration_check,
 	.transport_init = a2dp_mpeg_transport_init,
 	.transport_start = a2dp_mpeg_sink_transport_start,
 	.enabled = false,

commit c18e8123dab7222afcc08e5b0b5de87230e97a65

Fix validation for bidirectional FastStream setup
The configuration check was broken in a8b10fe.

src/a2dp-faststream.c
@@ -298,21 +298,19 @@ static int a2dp_faststream_configuration_check(
 				&conf_v, sizeof(conf_v)) != 0)
 		return A2DP_CHECK_ERR_SIZE;
 
-	switch (conf_v.direction) {
-	case FASTSTREAM_DIRECTION_MUSIC:
-	case FASTSTREAM_DIRECTION_VOICE:
-		break;
-	default:
+	if ((conf_v.direction & (FASTSTREAM_DIRECTION_MUSIC | FASTSTREAM_DIRECTION_VOICE)) == 0) {
 		debug("FastStream: Invalid direction: %#x", conf->direction);
 		return A2DP_CHECK_ERR_DIRECTIONS;
 	}
 
-	if (a2dp_sampling_lookup(a2dp_faststream_samplings_voice, conf_v.frequency_voice) == NULL) {
+	if (conf_v.direction & FASTSTREAM_DIRECTION_VOICE &&
+			a2dp_sampling_lookup(a2dp_faststream_samplings_voice, conf_v.frequency_voice) == NULL) {
 		debug("FastStream: Invalid voice sampling frequency: %#x", conf->frequency_voice);
 		return A2DP_CHECK_ERR_SAMPLING_VOICE;
 	}
 
-	if (a2dp_sampling_lookup(a2dp_faststream_samplings_music, conf_v.frequency_music) == NULL) {
+	if (conf_v.direction & FASTSTREAM_DIRECTION_MUSIC &&
+			a2dp_sampling_lookup(a2dp_faststream_samplings_music, conf_v.frequency_music) == NULL) {
 		debug("FastStream: Invalid music sampling frequency: %#x", conf->frequency_music);
 		return A2DP_CHECK_ERR_SAMPLING_MUSIC;
 	}

test/mock/mock.c
@@ -196,7 +196,7 @@ int main(int argc, char *argv[]) {
 
 	/* main loop with graceful termination handlers */
 	g_autoptr(GMainLoop) loop = g_main_loop_new(NULL, FALSE);
-	g_autoptr(GThread) loop_th = g_thread_new(NULL, mock_main_loop_run, loop);
+	GThread *loop_th = g_thread_new(NULL, mock_main_loop_run, loop);
 	g_timeout_add(timeout_ms, mock_sem_signal_handler, mock_sem_timeout);
 	g_unix_signal_add(SIGINT, mock_sem_signal_handler, mock_sem_timeout);
 	g_unix_signal_add(SIGTERM, mock_sem_signal_handler, mock_sem_timeout);

test/test-a2dp.c
@@ -24,6 +24,7 @@
 #include "a2dp.h"
 #include "a2dp-aac.h"
 #include "a2dp-aptx.h"
+#include "a2dp-faststream.h"
 #include "a2dp-sbc.h"
 #include "ba-config.h"
 #include "ba-transport.h"
@@ -172,6 +173,29 @@ CK_START_TEST(test_a2dp_check_configuration) {
 		.channels = AAC_CHANNELS_1 };
 	ck_assert_int_eq(a2dp_check_configuration(&a2dp_aac_source,
 			&cfg_aac_invalid, sizeof(cfg_aac_invalid)), A2DP_CHECK_ERR_OBJECT_TYPE);
+#endif
+
+#if ENABLE_FASTSTREAM
+
+	a2dp_faststream_t cfg_faststream = {
+		.info = A2DP_VENDOR_INFO_INIT(FASTSTREAM_VENDOR_ID, FASTSTREAM_CODEC_ID) };
+
+	/* FastStream codec requires at least one direction to be set. */
+	ck_assert_int_eq(a2dp_check_configuration(&a2dp_faststream_source,
+			&cfg_faststream, sizeof(cfg_faststream)), A2DP_CHECK_ERR_DIRECTIONS);
+
+	/* Check for valid unidirectional configuration. */
+	cfg_faststream.direction |= FASTSTREAM_DIRECTION_MUSIC;
+	cfg_faststream.frequency_music = FASTSTREAM_SAMPLING_FREQ_MUSIC_44100;
+	ck_assert_int_eq(a2dp_check_configuration(&a2dp_faststream_source,
+			&cfg_faststream, sizeof(cfg_faststream)), A2DP_CHECK_OK);
+
+	/* Check for valid bidirectional configuration. */
+	cfg_faststream.direction |= FASTSTREAM_DIRECTION_VOICE;
+	cfg_faststream.frequency_voice = FASTSTREAM_SAMPLING_FREQ_VOICE_16000;
+	ck_assert_int_eq(a2dp_check_configuration(&a2dp_faststream_source,
+			&cfg_faststream, sizeof(cfg_faststream)), A2DP_CHECK_OK);
+
 #endif
 
 } CK_END_TEST

test/test-utils-cli.c
@@ -74,7 +74,7 @@ CK_START_TEST(test_ba_service_not_running) {
 	char output[4096];
 
 	ck_assert_int_eq(run_bluealsa_cli(output, sizeof(output),
-				"status", NULL), EXIT_FAILURE);
+				"--dbus=test", "status", NULL), EXIT_FAILURE);
 
 } CK_END_TEST
 