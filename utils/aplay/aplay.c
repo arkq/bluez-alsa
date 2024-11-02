arkq/bluez-alsa

commit 0d488c7b671b0156f20a244a63ce1ab5e345afc7

Expose PCM channel map and per-channel volume
This commit changes the PCM Volume property type, and by doing that it
breaks compatibility with old BlueALSA clients.

utils/aplay/aplay.c
@@ -416,10 +416,10 @@ static int io_worker_mixer_volume_sync_ba_pcm(
 	if (!worker->mixer_has_mute_switch)
 		muted = pcm_muted;
 
-	ba_pcm->volume.ch1_muted = muted;
-	ba_pcm->volume.ch1_volume = volume;
-	ba_pcm->volume.ch2_muted = muted;
-	ba_pcm->volume.ch2_volume = volume;
+	for (size_t i = 0; i < ba_pcm->channels; i++) {
+		ba_pcm->volume[i].muted = muted;
+		ba_pcm->volume[i].volume = volume;
+	}
 
 	DBusError err = DBUS_ERROR_INIT;
 	if (!ba_dbus_pcm_update(&dbus_ctx, ba_pcm, BLUEALSA_PCM_VOLUME, &err)) {
@@ -452,20 +452,18 @@ static int io_worker_mixer_volume_sync_snd_mixer_elem(
 	 * some kind of channel mapping. In order to simplify things, we will set
 	 * all channels to the average left-right volume. */
 
-	const int vmax = BA_PCM_VOLUME_MAX(ba_pcm);
-	int volume = ba_pcm->volume.ch1_volume;
-	int muted = ba_pcm->volume.ch1_muted;
-
-	if (ba_pcm->channels > 1) {
-		volume = (ba_pcm->volume.ch1_volume + ba_pcm->volume.ch2_volume) / 2;
-		muted = ba_pcm->volume.ch1_muted || ba_pcm->volume.ch2_muted;
+	int volume_sum = 0, muted = 0;
+	for (size_t i = 1; i < ba_pcm->channels; i++) {
+		volume_sum += ba_pcm->volume[i].volume;
+		muted |= ba_pcm->volume[i].muted;
 	}
 
 	/* keep local muted state up to date */
 	pcm_muted = muted;
 
+	const int vmax = BA_PCM_VOLUME_MAX(ba_pcm);
 	/* convert loudness to dB using decibel formula */
-	long db = 10 * log2(1.0 * volume / vmax) * 100;
+	long db = 10 * log2(1.0 * volume_sum / ba_pcm->channels / vmax) * 100;
 	db += worker->mixer_volume_db_max_value;
 
 	int err;

src/a2dp-aac.c
@@ -39,27 +39,50 @@
 #include "shared/log.h"
 #include "shared/rt.h"
 
+static const enum ba_transport_pcm_channel a2dp_aac_channel_map_mono[] = {
+	BA_TRANSPORT_PCM_CHANNEL_MONO,
+};
+
+static const enum ba_transport_pcm_channel a2dp_aac_channel_map_stereo[] = {
+	BA_TRANSPORT_PCM_CHANNEL_FL, BA_TRANSPORT_PCM_CHANNEL_FR,
+};
+
+static const enum ba_transport_pcm_channel a2dp_aac_channel_map_5_1[] = {
+	BA_TRANSPORT_PCM_CHANNEL_FC,
+	BA_TRANSPORT_PCM_CHANNEL_FL, BA_TRANSPORT_PCM_CHANNEL_FR,
+	BA_TRANSPORT_PCM_CHANNEL_RL, BA_TRANSPORT_PCM_CHANNEL_RR,
+	BA_TRANSPORT_PCM_CHANNEL_LFE,
+};
+
+static const enum ba_transport_pcm_channel a2dp_aac_channel_map_7_1[] = {
+	BA_TRANSPORT_PCM_CHANNEL_FC,
+	BA_TRANSPORT_PCM_CHANNEL_FL, BA_TRANSPORT_PCM_CHANNEL_FR,
+	BA_TRANSPORT_PCM_CHANNEL_SL, BA_TRANSPORT_PCM_CHANNEL_SR,
+	BA_TRANSPORT_PCM_CHANNEL_RL, BA_TRANSPORT_PCM_CHANNEL_RR,
+	BA_TRANSPORT_PCM_CHANNEL_LFE,
+};
+
 static const struct a2dp_bit_mapping a2dp_aac_channels[] = {
-	{ AAC_CHANNEL_MODE_MONO, 1 },
-	{ AAC_CHANNEL_MODE_STEREO, 2 },
-	{ AAC_CHANNEL_MODE_5_1, 6 },
-	{ AAC_CHANNEL_MODE_7_1, 8 },
+	{ AAC_CHANNEL_MODE_MONO, .ch = { 1, a2dp_aac_channel_map_mono } },
+	{ AAC_CHANNEL_MODE_STEREO, .ch = { 2, a2dp_aac_channel_map_stereo } },
+	{ AAC_CHANNEL_MODE_5_1, .ch = { 6, a2dp_aac_channel_map_5_1 } },
+	{ AAC_CHANNEL_MODE_7_1, .ch = { 8, a2dp_aac_channel_map_7_1 } },
 	{ 0 }
 };
 
 static const struct a2dp_bit_mapping a2dp_aac_samplings[] = {
-	{ AAC_SAMPLING_FREQ_8000, 8000 },
-	{ AAC_SAMPLING_FREQ_11025, 11025 },
-	{ AAC_SAMPLING_FREQ_12000, 12000 },
-	{ AAC_SAMPLING_FREQ_16000, 16000 },
-	{ AAC_SAMPLING_FREQ_22050, 22050 },
-	{ AAC_SAMPLING_FREQ_24000, 24000 },
-	{ AAC_SAMPLING_FREQ_32000, 32000 },
-	{ AAC_SAMPLING_FREQ_44100, 44100 },
-	{ AAC_SAMPLING_FREQ_48000, 48000 },
-	{ AAC_SAMPLING_FREQ_64000, 64000 },
-	{ AAC_SAMPLING_FREQ_88200, 88200 },
-	{ AAC_SAMPLING_FREQ_96000, 96000 },
+	{ AAC_SAMPLING_FREQ_8000, { 8000 } },
+	{ AAC_SAMPLING_FREQ_11025, { 11025 } },
+	{ AAC_SAMPLING_FREQ_12000, { 12000 } },
+	{ AAC_SAMPLING_FREQ_16000, { 16000 } },
+	{ AAC_SAMPLING_FREQ_22050, { 22050 } },
+	{ AAC_SAMPLING_FREQ_24000, { 24000 } },
+	{ AAC_SAMPLING_FREQ_32000, { 32000 } },
+	{ AAC_SAMPLING_FREQ_44100, { 44100 } },
+	{ AAC_SAMPLING_FREQ_48000, { 48000 } },
+	{ AAC_SAMPLING_FREQ_64000, { 64000 } },
+	{ AAC_SAMPLING_FREQ_88200, { 88200 } },
+	{ AAC_SAMPLING_FREQ_96000, { 96000 } },
 	{ 0 }
 };
 
@@ -669,12 +692,12 @@ static int a2dp_aac_configuration_check(
 	}
 
 	const uint16_t conf_sampling_freq = A2DP_AAC_GET_SAMPLING_FREQ(conf_v);
-	if (a2dp_bit_mapping_lookup(a2dp_aac_samplings, conf_sampling_freq) == 0) {
+	if (a2dp_bit_mapping_lookup(a2dp_aac_samplings, conf_sampling_freq) == -1) {
 		debug("AAC: Invalid sampling frequency: %#x", A2DP_AAC_GET_SAMPLING_FREQ(*conf));
 		return A2DP_CHECK_ERR_SAMPLING;
 	}
 
-	if (a2dp_bit_mapping_lookup(a2dp_aac_channels, conf_v.channel_mode) == 0) {
+	if (a2dp_bit_mapping_lookup(a2dp_aac_channels, conf_v.channel_mode) == -1) {
 		debug("AAC: Invalid channel mode: %#x", conf->channel_mode);
 		return A2DP_CHECK_ERR_CHANNEL_MODE;
 	}
@@ -684,19 +707,22 @@ static int a2dp_aac_configuration_check(
 
 static int a2dp_aac_transport_init(struct ba_transport *t) {
 
-	unsigned int channels;
-	if ((channels = a2dp_bit_mapping_lookup(a2dp_aac_channels,
-					t->a2dp.configuration.aac.channel_mode)) == 0)
+	ssize_t channels_i;
+	if ((channels_i = a2dp_bit_mapping_lookup(a2dp_aac_channels,
+					t->a2dp.configuration.aac.channel_mode)) == -1)
 		return -1;
 
-	unsigned int sampling;
-	if ((sampling = a2dp_bit_mapping_lookup(a2dp_aac_samplings,
-					A2DP_AAC_GET_SAMPLING_FREQ(t->a2dp.configuration.aac))) == 0)
+	ssize_t sampling_i;
+	if ((sampling_i = a2dp_bit_mapping_lookup(a2dp_aac_samplings,
+					A2DP_AAC_GET_SAMPLING_FREQ(t->a2dp.configuration.aac))) == -1)
 		return -1;
 
 	t->a2dp.pcm.format = BA_TRANSPORT_PCM_FORMAT_S16_2LE;
-	t->a2dp.pcm.channels = channels;
-	t->a2dp.pcm.sampling = sampling;
+	t->a2dp.pcm.channels = a2dp_aac_channels[channels_i].value;
+	t->a2dp.pcm.sampling = a2dp_aac_samplings[sampling_i].value;
+
+	memcpy(t->a2dp.pcm.channel_map, a2dp_aac_channels[channels_i].ch.map,
+			t->a2dp.pcm.channels * sizeof(*t->a2dp.pcm.channel_map));
 
 	return 0;
 }

src/a2dp-aptx-hd.c
@@ -35,17 +35,25 @@
 #include "shared/log.h"
 #include "shared/rt.h"
 
+static const enum ba_transport_pcm_channel a2dp_aptx_channel_map_mono[] = {
+	BA_TRANSPORT_PCM_CHANNEL_MONO,
+};
+
+static const enum ba_transport_pcm_channel a2dp_aptx_channel_map_stereo[] = {
+	BA_TRANSPORT_PCM_CHANNEL_FL, BA_TRANSPORT_PCM_CHANNEL_FR,
+};
+
 static const struct a2dp_bit_mapping a2dp_aptx_channels[] = {
-	{ APTX_CHANNEL_MODE_MONO, 1 },
-	{ APTX_CHANNEL_MODE_STEREO, 2 },
+	{ APTX_CHANNEL_MODE_MONO, .ch = { 1, a2dp_aptx_channel_map_mono } },
+	{ APTX_CHANNEL_MODE_STEREO, .ch = { 2, a2dp_aptx_channel_map_stereo } },
 	{ 0 }
 };
 
 static const struct a2dp_bit_mapping a2dp_aptx_samplings[] = {
-	{ APTX_SAMPLING_FREQ_16000, 16000 },
-	{ APTX_SAMPLING_FREQ_32000, 32000 },
-	{ APTX_SAMPLING_FREQ_44100, 44100 },
-	{ APTX_SAMPLING_FREQ_48000, 48000 },
+	{ APTX_SAMPLING_FREQ_16000, { 16000 } },
+	{ APTX_SAMPLING_FREQ_32000, { 32000 } },
+	{ APTX_SAMPLING_FREQ_44100, { 44100 } },
+	{ APTX_SAMPLING_FREQ_48000, { 48000 } },
 	{ 0 }
 };
 
@@ -379,12 +387,12 @@ static int a2dp_aptx_hd_configuration_check(
 	/* Validate configuration against BlueALSA capabilities. */
 	a2dp_aptx_hd_caps_intersect(&conf_v, &sep->config.capabilities);
 
-	if (a2dp_bit_mapping_lookup(a2dp_aptx_samplings, conf_v.aptx.sampling_freq) == 0) {
+	if (a2dp_bit_mapping_lookup(a2dp_aptx_samplings, conf_v.aptx.sampling_freq) == -1) {
 		debug("apt-X HD: Invalid sampling frequency: %#x", conf->aptx.sampling_freq);
 		return A2DP_CHECK_ERR_SAMPLING;
 	}
 
-	if (a2dp_bit_mapping_lookup(a2dp_aptx_channels, conf_v.aptx.channel_mode) == 0) {
+	if (a2dp_bit_mapping_lookup(a2dp_aptx_channels, conf_v.aptx.channel_mode) == -1) {
 		debug("apt-X HD: Invalid channel mode: %#x", conf->aptx.channel_mode);
 		return A2DP_CHECK_ERR_CHANNEL_MODE;
 	}
@@ -394,19 +402,22 @@ static int a2dp_aptx_hd_configuration_check(
 
 static int a2dp_aptx_hd_transport_init(struct ba_transport *t) {
 
-	unsigned int channels;
-	if ((channels = a2dp_bit_mapping_lookup(a2dp_aptx_channels,
-					t->a2dp.configuration.aptx_hd.aptx.channel_mode)) == 0)
+	ssize_t channels_i;
+	if ((channels_i = a2dp_bit_mapping_lookup(a2dp_aptx_channels,
+					t->a2dp.configuration.aptx_hd.aptx.channel_mode)) == -1)
 		return -1;
 
-	unsigned int sampling;
-	if ((sampling = a2dp_bit_mapping_lookup(a2dp_aptx_samplings,
-					t->a2dp.configuration.aptx_hd.aptx.sampling_freq)) == 0)
+	ssize_t sampling_i;
+	if ((sampling_i = a2dp_bit_mapping_lookup(a2dp_aptx_samplings,
+					t->a2dp.configuration.aptx_hd.aptx.sampling_freq)) == -1)
 		return -1;
 
 	t->a2dp.pcm.format = BA_TRANSPORT_PCM_FORMAT_S24_4LE;
-	t->a2dp.pcm.channels = channels;
-	t->a2dp.pcm.sampling = sampling;
+	t->a2dp.pcm.channels = a2dp_aptx_channels[channels_i].value;
+	t->a2dp.pcm.sampling = a2dp_aptx_samplings[sampling_i].value;
+
+	memcpy(t->a2dp.pcm.channel_map, a2dp_aptx_channels[channels_i].ch.map,
+			t->a2dp.pcm.channels * sizeof(*t->a2dp.pcm.channel_map));
 
 	return 0;
 }

src/a2dp-aptx.c
@@ -33,17 +33,25 @@
 #include "shared/log.h"
 #include "shared/rt.h"
 
+static const enum ba_transport_pcm_channel a2dp_aptx_channel_map_mono[] = {
+	BA_TRANSPORT_PCM_CHANNEL_MONO,
+};
+
+static const enum ba_transport_pcm_channel a2dp_aptx_channel_map_stereo[] = {
+	BA_TRANSPORT_PCM_CHANNEL_FL, BA_TRANSPORT_PCM_CHANNEL_FR,
+};
+
 static const struct a2dp_bit_mapping a2dp_aptx_channels[] = {
-	{ APTX_CHANNEL_MODE_MONO, 1 },
-	{ APTX_CHANNEL_MODE_STEREO, 2 },
+	{ APTX_CHANNEL_MODE_MONO, .ch = { 1, a2dp_aptx_channel_map_mono } },
+	{ APTX_CHANNEL_MODE_STEREO, .ch = { 2, a2dp_aptx_channel_map_stereo } },
 	{ 0 }
 };
 
 static const struct a2dp_bit_mapping a2dp_aptx_samplings[] = {
-	{ APTX_SAMPLING_FREQ_16000, 16000 },
-	{ APTX_SAMPLING_FREQ_32000, 32000 },
-	{ APTX_SAMPLING_FREQ_44100, 44100 },
-	{ APTX_SAMPLING_FREQ_48000, 48000 },
+	{ APTX_SAMPLING_FREQ_16000, { 16000 } },
+	{ APTX_SAMPLING_FREQ_32000, { 32000 } },
+	{ APTX_SAMPLING_FREQ_44100, { 44100 } },
+	{ APTX_SAMPLING_FREQ_48000, { 48000 } },
 	{ 0 }
 };
 
@@ -341,12 +349,12 @@ static int a2dp_aptx_configuration_check(
 	/* Validate configuration against BlueALSA capabilities. */
 	a2dp_aptx_caps_intersect(&conf_v, &sep->config.capabilities);
 
-	if (a2dp_bit_mapping_lookup(a2dp_aptx_samplings, conf_v.sampling_freq) == 0) {
+	if (a2dp_bit_mapping_lookup(a2dp_aptx_samplings, conf_v.sampling_freq) == -1) {
 		debug("apt-X: Invalid sampling frequency: %#x", conf->sampling_freq);
 		return A2DP_CHECK_ERR_SAMPLING;
 	}
 
-	if (a2dp_bit_mapping_lookup(a2dp_aptx_channels, conf_v.channel_mode) == 0) {
+	if (a2dp_bit_mapping_lookup(a2dp_aptx_channels, conf_v.channel_mode) == -1) {
 		debug("apt-X: Invalid channel mode: %#x", conf->channel_mode);
 		return A2DP_CHECK_ERR_CHANNEL_MODE;
 	}
@@ -356,19 +364,22 @@ static int a2dp_aptx_configuration_check(
 
 static int a2dp_aptx_transport_init(struct ba_transport *t) {
 
-	unsigned int channels;
-	if ((channels = a2dp_bit_mapping_lookup(a2dp_aptx_channels,
-					t->a2dp.configuration.aptx.channel_mode)) == 0)
+	ssize_t channels_i;
+	if ((channels_i = a2dp_bit_mapping_lookup(a2dp_aptx_channels,
+					t->a2dp.configuration.aptx.channel_mode)) == -1)
 		return -1;
 
-	unsigned int sampling;
-	if ((sampling = a2dp_bit_mapping_lookup(a2dp_aptx_samplings,
-					t->a2dp.configuration.aptx.sampling_freq)) == 0)
+	ssize_t sampling_i;
+	if ((sampling_i = a2dp_bit_mapping_lookup(a2dp_aptx_samplings,
+					t->a2dp.configuration.aptx.sampling_freq)) == -1)
 		return -1;
 
 	t->a2dp.pcm.format = BA_TRANSPORT_PCM_FORMAT_S16_2LE;
-	t->a2dp.pcm.channels = channels;
-	t->a2dp.pcm.sampling = sampling;
+	t->a2dp.pcm.channels = a2dp_aptx_channels[channels_i].value;
+	t->a2dp.pcm.sampling = a2dp_aptx_samplings[sampling_i].value;
+
+	memcpy(t->a2dp.pcm.channel_map, a2dp_aptx_channels[channels_i].ch.map,
+			t->a2dp.pcm.channels * sizeof(*t->a2dp.pcm.channel_map));
 
 	return 0;
 }

src/a2dp-faststream.c
@@ -33,13 +33,13 @@
 #include "shared/rt.h"
 
 static const struct a2dp_bit_mapping a2dp_faststream_samplings_music[] = {
-	{ FASTSTREAM_SAMPLING_FREQ_MUSIC_44100, 44100 },
-	{ FASTSTREAM_SAMPLING_FREQ_MUSIC_48000, 48000 },
+	{ FASTSTREAM_SAMPLING_FREQ_MUSIC_44100, { 44100 } },
+	{ FASTSTREAM_SAMPLING_FREQ_MUSIC_48000, { 48000 } },
 	{ 0 }
 };
 
 static const struct a2dp_bit_mapping a2dp_faststream_samplings_voice[] = {
-	{ FASTSTREAM_SAMPLING_FREQ_VOICE_16000, 16000 },
+	{ FASTSTREAM_SAMPLING_FREQ_VOICE_16000, { 16000 } },
 	{ 0 }
 };
 
@@ -374,13 +374,13 @@ static int a2dp_faststream_configuration_check(
 	}
 
 	if (conf_v.direction & FASTSTREAM_DIRECTION_VOICE &&
-			a2dp_bit_mapping_lookup(a2dp_faststream_samplings_voice, conf_v.sampling_freq_voice) == 0) {
+			a2dp_bit_mapping_lookup(a2dp_faststream_samplings_voice, conf_v.sampling_freq_voice) == -1) {
 		debug("FastStream: Invalid voice sampling frequency: %#x", conf->sampling_freq_voice);
 		return A2DP_CHECK_ERR_SAMPLING_VOICE;
 	}
 
 	if (conf_v.direction & FASTSTREAM_DIRECTION_MUSIC &&
-			a2dp_bit_mapping_lookup(a2dp_faststream_samplings_music, conf_v.sampling_freq_music) == 0) {
+			a2dp_bit_mapping_lookup(a2dp_faststream_samplings_music, conf_v.sampling_freq_music) == -1) {
 		debug("FastStream: Invalid music sampling frequency: %#x", conf->sampling_freq_music);
 		return A2DP_CHECK_ERR_SAMPLING_MUSIC;
 	}
@@ -392,27 +392,32 @@ static int a2dp_faststream_transport_init(struct ba_transport *t) {
 
 	if (t->a2dp.configuration.faststream.direction & FASTSTREAM_DIRECTION_MUSIC) {
 
-		unsigned int sampling;
-		if ((sampling = a2dp_bit_mapping_lookup(a2dp_faststream_samplings_music,
-						t->a2dp.configuration.faststream.sampling_freq_music)) == 0)
+		ssize_t sampling_i;
+		if ((sampling_i = a2dp_bit_mapping_lookup(a2dp_faststream_samplings_music,
+						t->a2dp.configuration.faststream.sampling_freq_music)) == -1)
 			return -1;
 
 		t->a2dp.pcm.format = BA_TRANSPORT_PCM_FORMAT_S16_2LE;
-		t->a2dp.pcm.sampling = sampling;
 		t->a2dp.pcm.channels = 2;
+		t->a2dp.pcm.sampling = a2dp_faststream_samplings_music[sampling_i].value;
+
+		t->a2dp.pcm.channel_map[0] = BA_TRANSPORT_PCM_CHANNEL_FL;
+		t->a2dp.pcm.channel_map[1] = BA_TRANSPORT_PCM_CHANNEL_FR;
 
 	}
 
 	if (t->a2dp.configuration.faststream.direction & FASTSTREAM_DIRECTION_VOICE) {
 
-		unsigned int sampling;
-		if ((sampling = a2dp_bit_mapping_lookup(a2dp_faststream_samplings_voice,
-						t->a2dp.configuration.faststream.sampling_freq_voice)) == 0)
+		ssize_t sampling_i;
+		if ((sampling_i = a2dp_bit_mapping_lookup(a2dp_faststream_samplings_voice,
+						t->a2dp.configuration.faststream.sampling_freq_voice)) == -1)
 			return -1;
 
 		t->a2dp.pcm_bc.format = BA_TRANSPORT_PCM_FORMAT_S16_2LE;
-		t->a2dp.pcm_bc.sampling = sampling;
 		t->a2dp.pcm_bc.channels = 1;
+		t->a2dp.pcm_bc.sampling = a2dp_faststream_samplings_voice[sampling_i].value;
+
+		t->a2dp.pcm_bc.channel_map[0] = BA_TRANSPORT_PCM_CHANNEL_MONO;
 
 	}
 

src/a2dp-lc3plus.c
@@ -39,15 +39,23 @@
 #include "shared/log.h"
 #include "shared/rt.h"
 
+static const enum ba_transport_pcm_channel a2dp_lc3plus_channel_map_mono[] = {
+	BA_TRANSPORT_PCM_CHANNEL_MONO,
+};
+
+static const enum ba_transport_pcm_channel a2dp_lc3plus_channel_map_stereo[] = {
+	BA_TRANSPORT_PCM_CHANNEL_FL, BA_TRANSPORT_PCM_CHANNEL_FR,
+};
+
 static const struct a2dp_bit_mapping a2dp_lc3plus_channels[] = {
-	{ LC3PLUS_CHANNEL_MODE_MONO, 1 },
-	{ LC3PLUS_CHANNEL_MODE_STEREO, 2 },
+	{ LC3PLUS_CHANNEL_MODE_MONO, .ch = { 1, a2dp_lc3plus_channel_map_mono } },
+	{ LC3PLUS_CHANNEL_MODE_STEREO, .ch = { 2, a2dp_lc3plus_channel_map_stereo } },
 	{ 0 }
 };
 
 static const struct a2dp_bit_mapping a2dp_lc3plus_samplings[] = {
-	{ LC3PLUS_SAMPLING_FREQ_48000, 48000 },
-	{ LC3PLUS_SAMPLING_FREQ_96000, 96000 },
+	{ LC3PLUS_SAMPLING_FREQ_48000, { 48000 } },
+	{ LC3PLUS_SAMPLING_FREQ_96000, { 96000 } },
 	{ 0 }
 };
 
@@ -669,13 +677,13 @@ static int a2dp_lc3plus_configuration_check(
 		return A2DP_CHECK_ERR_FRAME_DURATION;
 	}
 
-	if (a2dp_bit_mapping_lookup(a2dp_lc3plus_channels, conf_v.channel_mode) == 0) {
+	if (a2dp_bit_mapping_lookup(a2dp_lc3plus_channels, conf_v.channel_mode) == -1) {
 		debug("LC3plus: Invalid channel mode: %#x", conf->channel_mode);
 		return A2DP_CHECK_ERR_CHANNEL_MODE;
 	}
 
 	uint16_t conf_sampling_freq = A2DP_LC3PLUS_GET_SAMPLING_FREQ(conf_v);
-	if (a2dp_bit_mapping_lookup(a2dp_lc3plus_samplings, conf_sampling_freq) == 0) {
+	if (a2dp_bit_mapping_lookup(a2dp_lc3plus_samplings, conf_sampling_freq) == -1) {
 		debug("LC3plus: Invalid sampling frequency: %#x", A2DP_LC3PLUS_GET_SAMPLING_FREQ(*conf));
 		return A2DP_CHECK_ERR_SAMPLING;
 	}
@@ -685,19 +693,22 @@ static int a2dp_lc3plus_configuration_check(
 
 static int a2dp_lc3plus_transport_init(struct ba_transport *t) {
 
-	unsigned int channels;
-	if ((channels = a2dp_bit_mapping_lookup(a2dp_lc3plus_channels,
-					t->a2dp.configuration.lc3plus.channel_mode)) == 0)
+	ssize_t channels_i;
+	if ((channels_i = a2dp_bit_mapping_lookup(a2dp_lc3plus_channels,
+					t->a2dp.configuration.lc3plus.channel_mode)) == -1)
 		return -1;
 
-	unsigned int sampling;
-	if ((sampling = a2dp_bit_mapping_lookup(a2dp_lc3plus_samplings,
-					A2DP_LC3PLUS_GET_SAMPLING_FREQ(t->a2dp.configuration.lc3plus))) == 0)
+	ssize_t sampling_i;
+	if ((sampling_i = a2dp_bit_mapping_lookup(a2dp_lc3plus_samplings,
+					A2DP_LC3PLUS_GET_SAMPLING_FREQ(t->a2dp.configuration.lc3plus))) == -1)
 		return -1;
 
 	t->a2dp.pcm.format = BA_TRANSPORT_PCM_FORMAT_S24_4LE;
-	t->a2dp.pcm.channels = channels;
-	t->a2dp.pcm.sampling = sampling;
+	t->a2dp.pcm.channels = a2dp_lc3plus_channels[channels_i].value;
+	t->a2dp.pcm.sampling = a2dp_lc3plus_samplings[sampling_i].value;
+
+	memcpy(t->a2dp.pcm.channel_map, a2dp_lc3plus_channels[channels_i].ch.map,
+			t->a2dp.pcm.channels * sizeof(*t->a2dp.pcm.channel_map));
 
 	return 0;
 }

src/a2dp-ldac.c
@@ -36,20 +36,28 @@
 #include "shared/log.h"
 #include "shared/rt.h"
 
+static const enum ba_transport_pcm_channel a2dp_ldac_channel_map_mono[] = {
+	BA_TRANSPORT_PCM_CHANNEL_MONO,
+};
+
+static const enum ba_transport_pcm_channel a2dp_ldac_channel_map_stereo[] = {
+	BA_TRANSPORT_PCM_CHANNEL_FL, BA_TRANSPORT_PCM_CHANNEL_FR,
+};
+
 static const struct a2dp_bit_mapping a2dp_ldac_channels[] = {
-	{ LDAC_CHANNEL_MODE_MONO, 1 },
-	{ LDAC_CHANNEL_MODE_DUAL, 2 },
-	{ LDAC_CHANNEL_MODE_STEREO, 2 },
+	{ LDAC_CHANNEL_MODE_MONO, .ch = { 1, a2dp_ldac_channel_map_mono } },
+	{ LDAC_CHANNEL_MODE_DUAL, .ch = { 2, a2dp_ldac_channel_map_stereo } },
+	{ LDAC_CHANNEL_MODE_STEREO, .ch = { 2, a2dp_ldac_channel_map_stereo } },
 	{ 0 }
 };
 
 static const struct a2dp_bit_mapping a2dp_ldac_samplings[] = {
-	{ LDAC_SAMPLING_FREQ_44100, 44100 },
-	{ LDAC_SAMPLING_FREQ_48000, 48000 },
-	{ LDAC_SAMPLING_FREQ_88200, 88200 },
-	{ LDAC_SAMPLING_FREQ_96000, 96000 },
-	{ LDAC_SAMPLING_FREQ_176400, 176400 },
-	{ LDAC_SAMPLING_FREQ_192000, 192000 },
+	{ LDAC_SAMPLING_FREQ_44100, { 44100 } },
+	{ LDAC_SAMPLING_FREQ_48000, { 48000 } },
+	{ LDAC_SAMPLING_FREQ_88200, { 88200 } },
+	{ LDAC_SAMPLING_FREQ_96000, { 96000 } },
+	{ LDAC_SAMPLING_FREQ_176400, { 176400 } },
+	{ LDAC_SAMPLING_FREQ_192000, { 192000 } },
 	{ 0 }
 };
 
@@ -435,12 +443,12 @@ static int a2dp_ldac_configuration_check(
 	/* Validate configuration against BlueALSA capabilities. */
 	a2dp_ldac_caps_intersect(&conf_v, &sep->config.capabilities);
 
-	if (a2dp_bit_mapping_lookup(a2dp_ldac_samplings, conf_v.sampling_freq) == 0) {
+	if (a2dp_bit_mapping_lookup(a2dp_ldac_samplings, conf_v.sampling_freq) == -1) {
 		debug("LDAC: Invalid sampling frequency: %#x", conf->sampling_freq);
 		return A2DP_CHECK_ERR_SAMPLING;
 	}
 
-	if (a2dp_bit_mapping_lookup(a2dp_ldac_channels, conf_v.channel_mode) == 0) {
+	if (a2dp_bit_mapping_lookup(a2dp_ldac_channels, conf_v.channel_mode) == -1) {
 		debug("LDAC: Invalid channel mode: %#x", conf->channel_mode);
 		return A2DP_CHECK_ERR_CHANNEL_MODE;
 	}
@@ -450,21 +458,24 @@ static int a2dp_ldac_configuration_check(
 
 static int a2dp_ldac_transport_init(struct ba_transport *t) {
 
-	unsigned int channels;
-	if ((channels = a2dp_bit_mapping_lookup(a2dp_ldac_channels,
-					t->a2dp.configuration.ldac.channel_mode)) == 0)
+	ssize_t channels_i;
+	if ((channels_i = a2dp_bit_mapping_lookup(a2dp_ldac_channels,
+					t->a2dp.configuration.ldac.channel_mode)) == -1)
 		return -1;
 
-	unsigned int sampling;
-	if ((sampling = a2dp_bit_mapping_lookup(a2dp_ldac_samplings,
-					t->a2dp.configuration.ldac.sampling_freq)) == 0)
+	ssize_t sampling_i;
+	if ((sampling_i = a2dp_bit_mapping_lookup(a2dp_ldac_samplings,
+					t->a2dp.configuration.ldac.sampling_freq)) == -1)
 		return -1;
 
 	/* LDAC library internally for encoding uses 31-bit integers or
 	 * floats, so the best choice for PCM sample is signed 32-bit. */
 	t->a2dp.pcm.format = BA_TRANSPORT_PCM_FORMAT_S32_4LE;
-	t->a2dp.pcm.channels = channels;
-	t->a2dp.pcm.sampling = sampling;
+	t->a2dp.pcm.channels = a2dp_ldac_channels[channels_i].value;
+	t->a2dp.pcm.sampling = a2dp_ldac_samplings[sampling_i].value;
+
+	memcpy(t->a2dp.pcm.channel_map, a2dp_ldac_channels[channels_i].ch.map,
+			t->a2dp.pcm.channels * sizeof(*t->a2dp.pcm.channel_map));
 
 	return 0;
 }

src/a2dp-mpeg.c
@@ -44,21 +44,29 @@
 #include "shared/log.h"
 #include "shared/rt.h"
 
+static const enum ba_transport_pcm_channel a2dp_mpeg_channel_map_mono[] = {
+	BA_TRANSPORT_PCM_CHANNEL_MONO,
+};
+
+static const enum ba_transport_pcm_channel a2dp_mpeg_channel_map_stereo[] = {
+	BA_TRANSPORT_PCM_CHANNEL_FL, BA_TRANSPORT_PCM_CHANNEL_FR,
+};
+
 static const struct a2dp_bit_mapping a2dp_mpeg_channels[] = {
-	{ MPEG_CHANNEL_MODE_MONO, 1 },
-	{ MPEG_CHANNEL_MODE_DUAL_CHANNEL, 2 },
-	{ MPEG_CHANNEL_MODE_STEREO, 2 },
-	{ MPEG_CHANNEL_MODE_JOINT_STEREO, 2 },
+	{ MPEG_CHANNEL_MODE_MONO, .ch = { 1, a2dp_mpeg_channel_map_mono } },
+	{ MPEG_CHANNEL_MODE_DUAL_CHANNEL, .ch = { 2, a2dp_mpeg_channel_map_stereo } },
+	{ MPEG_CHANNEL_MODE_STEREO, .ch = { 2, a2dp_mpeg_channel_map_stereo } },
+	{ MPEG_CHANNEL_MODE_JOINT_STEREO, .ch = { 2, a2dp_mpeg_channel_map_stereo } },
 	{ 0 }
 };
 
 static const struct a2dp_bit_mapping a2dp_mpeg_samplings[] = {
-	{ MPEG_SAMPLING_FREQ_16000, 16000 },
-	{ MPEG_SAMPLING_FREQ_22050, 22050 },
-	{ MPEG_SAMPLING_FREQ_24000, 24000 },
-	{ MPEG_SAMPLING_FREQ_32000, 32000 },
-	{ MPEG_SAMPLING_FREQ_44100, 44100 },
-	{ MPEG_SAMPLING_FREQ_48000, 48000 },
+	{ MPEG_SAMPLING_FREQ_16000, { 16000 } },
+	{ MPEG_SAMPLING_FREQ_22050, { 22050 } },
+	{ MPEG_SAMPLING_FREQ_24000, { 24000 } },
+	{ MPEG_SAMPLING_FREQ_32000, { 32000 } },
+	{ MPEG_SAMPLING_FREQ_44100, { 44100 } },
+	{ MPEG_SAMPLING_FREQ_48000, { 48000 } },
 	{ 0 }
 };
 
@@ -595,12 +603,12 @@ static int a2dp_mpeg_configuration_check(
 		return A2DP_CHECK_ERR_MPEG_LAYER;
 	}
 
-	if (a2dp_bit_mapping_lookup(a2dp_mpeg_channels, conf_v.channel_mode) == 0) {
+	if (a2dp_bit_mapping_lookup(a2dp_mpeg_channels, conf_v.channel_mode) == -1) {
 		debug("MPEG: Invalid channel mode: %#x", conf->channel_mode);
 		return A2DP_CHECK_ERR_CHANNEL_MODE;
 	}
 
-	if (a2dp_bit_mapping_lookup(a2dp_mpeg_samplings, conf_v.sampling_freq) == 0) {
+	if (a2dp_bit_mapping_lookup(a2dp_mpeg_samplings, conf_v.sampling_freq) == -1) {
 		debug("MPEG: Invalid sampling frequency: %#x", conf->sampling_freq);
 		return A2DP_CHECK_ERR_SAMPLING;
 	}
@@ -610,19 +618,22 @@ static int a2dp_mpeg_configuration_check(
 
 static int a2dp_mpeg_transport_init(struct ba_transport *t) {
 
-	unsigned int channels;
-	if ((channels = a2dp_bit_mapping_lookup(a2dp_mpeg_channels,
-					t->a2dp.configuration.mpeg.channel_mode)) == 0)
+	ssize_t channels_i;
+	if ((channels_i = a2dp_bit_mapping_lookup(a2dp_mpeg_channels,
+					t->a2dp.configuration.mpeg.channel_mode)) == -1)
 		return -1;
 
-	unsigned int sampling;
-	if ((sampling = a2dp_bit_mapping_lookup(a2dp_mpeg_samplings,
-					t->a2dp.configuration.mpeg.sampling_freq)) == 0)
+	ssize_t sampling_i;
+	if ((sampling_i = a2dp_bit_mapping_lookup(a2dp_mpeg_samplings,
+					t->a2dp.configuration.mpeg.sampling_freq)) == -1)
 		return -1;
 
 	t->a2dp.pcm.format = BA_TRANSPORT_PCM_FORMAT_S16_2LE;
-	t->a2dp.pcm.channels = channels;
-	t->a2dp.pcm.sampling = sampling;
+	t->a2dp.pcm.channels = a2dp_mpeg_channels[channels_i].value;
+	t->a2dp.pcm.sampling = a2dp_mpeg_samplings[sampling_i].value;
+
+	memcpy(t->a2dp.pcm.channel_map, a2dp_mpeg_channels[channels_i].ch.map,
+			t->a2dp.pcm.channels * sizeof(*t->a2dp.pcm.channel_map));
 
 	return 0;
 }

commit b9db5b697a40728635377c7e6a662d6cde6b3afa

Fix aplay output volume scaling caused by 0d488c7

utils/aplay/aplay.c
@@ -455,7 +455,7 @@ static int io_worker_mixer_volume_sync_snd_mixer_elem(
 	 * all channels to the average left-right volume. */
 
 	unsigned int volume_sum = 0, muted = 0;
-	for (size_t i = 1; i < ba_pcm->channels; i++) {
+	for (size_t i = 0; i < ba_pcm->channels; i++) {
 		volume_sum += ba_pcm->volume[i].volume;
 		muted |= ba_pcm->volume[i].muted;
 	}