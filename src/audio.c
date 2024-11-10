arkq/bluez-alsa

commit 45af1a782464e88baa6b3803916b9725c797d4b6

Hint compiler about memory aliasing restrictions

src/audio.c
@@ -35,8 +35,8 @@ double audio_loudness_to_decibel(double value) {
 
 /**
  * Join channels into interleaved S16 PCM signal. */
-void audio_interleave_s16_2le(int16_t *dest, const int16_t **src,
-		unsigned int channels, size_t frames) {
+void audio_interleave_s16_2le(int16_t * restrict dest,
+		const int16_t * restrict * restrict src, unsigned int channels, size_t frames) {
 	for (size_t f = 0; f < frames; f++)
 		for (size_t c = 0; c < channels; c++)
 			*dest++ = src[c][f];
@@ -44,8 +44,8 @@ void audio_interleave_s16_2le(int16_t *dest, const int16_t **src,
 
 /**
  * Join channels into interleaved S32 PCM signal. */
-void audio_interleave_s32_4le(int32_t *dest, const int32_t **src,
-		unsigned int channels, size_t frames) {
+void audio_interleave_s32_4le(int32_t * restrict dest,
+		const int32_t * restrict * restrict src, unsigned int channels, size_t frames) {
 	for (size_t f = 0; f < frames; f++)
 		for (size_t c = 0; c < channels; c++)
 			*dest++ = src[c][f];
@@ -53,8 +53,8 @@ void audio_interleave_s32_4le(int32_t *dest, const int32_t **src,
 
 /**
  * Split interleaved S16 PCM signal into channels. */
-void audio_deinterleave_s16_2le(int16_t **dest, const int16_t *src,
-		unsigned int channels, size_t frames) {
+void audio_deinterleave_s16_2le(int16_t * restrict * restrict dest,
+		const int16_t * restrict src, unsigned int channels, size_t frames) {
 	for (size_t f = 0; f < frames; f++)
 		for (size_t c = 0; c < channels; c++)
 			dest[c][f] = *src++;
@@ -62,8 +62,8 @@ void audio_deinterleave_s16_2le(int16_t **dest, const int16_t *src,
 
 /**
  * Split interleaved S32 PCM signal into channels. */
-void audio_deinterleave_s32_4le(int32_t **dest, const int32_t *src,
-		unsigned int channels, size_t frames) {
+void audio_deinterleave_s32_4le(int32_t * restrict * restrict dest,
+		const int32_t * restrict src, unsigned int channels, size_t frames) {
 	for (size_t f = 0; f < frames; f++)
 		for (size_t c = 0; c < channels; c++)
 			dest[c][f] = *src++;
@@ -80,24 +80,18 @@ void audio_deinterleave_s32_4le(int32_t **dest, const int32_t *src,
  * @param scale The scaling factor per channel for the PCM signal.
  * @param channels The number of channels in the buffer.
  * @param frames The number of PCM frames in the buffer. */
-void audio_scale_s16_2le(int16_t *buffer, const double *scale,
-		unsigned int channels, size_t frames) {
-	size_t i = 0;
-	while (frames--)
-		for (size_t ii = 0; ii < channels; ii++) {
-			int16_t s = (int16_t)le16toh(buffer[i]) * scale[ii];
-			buffer[i++] = htole16(s);
-		}
+void audio_scale_s16_2le(int16_t * restrict buffer,
+		const double * restrict scale, unsigned int channels, size_t frames) {
+	for (size_t i = 0; frames; frames--)
+		for (size_t c = 0; c < channels; c++, i++)
+			buffer[i] = htole16(((int16_t)le16toh(buffer[i]) * scale[c]));
 }
 
 /**
  * Scale S32_4LE PCM signal. */
-void audio_scale_s32_4le(int32_t *buffer, const double *scale,
-		unsigned int channels, size_t frames) {
-	size_t i = 0;
-	while (frames--)
-		for (size_t ii = 0; ii < channels; ii++) {
-			int32_t s = (int32_t)le32toh(buffer[i]) * scale[ii];
-			buffer[i++] = htole32(s);
-		}
+void audio_scale_s32_4le(int32_t * restrict buffer,
+		const double * restrict scale, unsigned int channels, size_t frames) {
+	for (size_t i = 0; frames; frames--)
+		for (size_t c = 0; c < channels; c++, i++)
+			buffer[i] = htole32((int32_t)le32toh(buffer[i]) * scale[c]);
 }

src/a2dp.c
@@ -156,8 +156,8 @@ uint32_t a2dp_bit_mapping_lookup_value(
  * This function performs a simple bitwise AND operation on given capabilities
  * and mask. */
 void a2dp_caps_bitwise_intersect(
-		void *capabilities,
-		const void *mask,
+		void * restrict capabilities,
+		const void * restrict mask,
 		size_t size) {
 
 	const uint8_t *caps_mask = mask;

src/a2dp.h
@@ -86,8 +86,8 @@ enum a2dp_stream {
 };
 
 void a2dp_caps_bitwise_intersect(
-		void *capabilities,
-		const void *mask,
+		void * restrict capabilities,
+		const void * restrict mask,
 		size_t size);
 
 bool a2dp_caps_has_main_stream_only(

src/audio.h
@@ -22,21 +22,21 @@
 double audio_decibel_to_loudness(double value);
 double audio_loudness_to_decibel(double value);
 
-void audio_interleave_s16_2le(int16_t *dest, const int16_t **src,
-		unsigned int channels, size_t frames);
-void audio_interleave_s32_4le(int32_t *dest, const int32_t **src,
-		unsigned int channels, size_t frames);
+void audio_interleave_s16_2le(int16_t * restrict dest,
+		const int16_t * restrict * restrict src, unsigned int channels, size_t frames);
+void audio_interleave_s32_4le(int32_t * restrict dest,
+		const int32_t * restrict * restrict src, unsigned int channels, size_t frames);
 #define audio_interleave_s24_4le audio_interleave_s32_4le
 
-void audio_deinterleave_s16_2le(int16_t **dest, const int16_t *src,
-		unsigned int channels, size_t frames);
-void audio_deinterleave_s32_4le(int32_t **dest, const int32_t *src,
-		unsigned int channels, size_t frames);
+void audio_deinterleave_s16_2le(int16_t * restrict * restrict dest,
+		const int16_t * restrict src, unsigned int channels, size_t frames);
+void audio_deinterleave_s32_4le(int32_t * restrict * restrict dest,
+		const int32_t * restrict src, unsigned int channels, size_t frames);
 #define audio_deinterleave_s24_4le audio_deinterleave_s32_4le
 
-void audio_scale_s16_2le(int16_t *buffer, const double *scale,
+void audio_scale_s16_2le(int16_t * restrict buffer, const double * restrict scale,
 		unsigned int channels, size_t frames);
-void audio_scale_s32_4le(int32_t *buffer, const double *scale,
+void audio_scale_s32_4le(int32_t * restrict buffer, const double * restrict scale,
 		unsigned int channels, size_t frames);
 #define audio_scale_s24_4le audio_scale_s32_4le
 

src/bluealsactl/cmd-open.c
@@ -100,7 +100,8 @@ static int cmd_open_func(int argc, char *argv[]) {
 	}
 
 	uint8_t buffer[4096];
-	uint8_t buffer_hex[sizeof(buffer) * 2 + 1];
+	uint8_t buffer_bin[sizeof(buffer) / 2];
+	char buffer_hex[sizeof(buffer) * 2 + 1];
 	ssize_t count;
 
 	while ((count = read(fd_input, buffer, sizeof(buffer))) > 0) {
@@ -111,15 +112,16 @@ static int cmd_open_func(int argc, char *argv[]) {
 		if (hex) {
 
 			if (fd_input == STDIN_FILENO) {
-				if ((count = hex2bin((const char *)buffer, buffer, count)) == -1) {
+				pos = buffer_bin;
+				if ((count = hex2bin((const char *)buffer, buffer_bin, count)) == -1) {
 					cmd_print_error("Couldn't decode hex string: %s", strerror(errno));
 					continue;
 				}
 			}
 
 			if (fd_output == STDOUT_FILENO) {
-				count = bin2hex(buffer, (char *)buffer_hex, count);
-				pos = buffer_hex;
+				pos = (const uint8_t *)buffer_hex;
+				count = bin2hex(buffer, buffer_hex, count);
 			}
 
 		}

src/shared/dbus-client-pcm.c
@@ -214,8 +214,9 @@ const char *ba_dbus_pcm_codec_get_canonical_name(
 	return a2dp_codecs_get_canonical_name(alias);
 }
 
-static void dbus_message_iter_get_codec_data(DBusMessageIter *variant,
-		struct ba_pcm_codec *codec) {
+static void dbus_message_iter_get_codec_data(
+		DBusMessageIter * restrict variant,
+		struct ba_pcm_codec * restrict codec) {
 
 	DBusMessageIter iter;
 	unsigned char *data;
@@ -229,8 +230,9 @@ static void dbus_message_iter_get_codec_data(DBusMessageIter *variant,
 
 }
 
-static void dbus_message_iter_get_codec_channels(DBusMessageIter *variant,
-		struct ba_pcm_codec *codec) {
+static void dbus_message_iter_get_codec_channels(
+		DBusMessageIter * restrict variant,
+		struct ba_pcm_codec * restrict codec) {
 
 	DBusMessageIter iter;
 	unsigned char *data;
@@ -245,8 +247,9 @@ static void dbus_message_iter_get_codec_channels(DBusMessageIter *variant,
 
 }
 
-static void dbus_message_iter_get_codec_rates(DBusMessageIter *variant,
-		struct ba_pcm_codec *codec) {
+static void dbus_message_iter_get_codec_rates(
+		DBusMessageIter * restrict variant,
+		struct ba_pcm_codec * restrict codec) {
 
 	DBusMessageIter iter;
 	dbus_uint32_t *data;
@@ -261,8 +264,9 @@ static void dbus_message_iter_get_codec_rates(DBusMessageIter *variant,
 
 }
 
-static void dbus_message_iter_get_codec_channel_maps(DBusMessageIter *variant,
-		struct ba_pcm_codec *codec) {
+static void dbus_message_iter_get_codec_channel_maps(
+		DBusMessageIter * restrict variant,
+		struct ba_pcm_codec * restrict codec) {
 
 	size_t i;
 	DBusMessageIter iter_array;

src/shared/hex.c
@@ -21,7 +21,7 @@
  *   data.
  * @param n The length of the binary buffer which shall be encoded.
  * @return This function returns length of the hex string. */
-ssize_t bin2hex(const void *bin, char *hex, size_t n) {
+ssize_t bin2hex(const void * restrict bin, char * restrict hex, size_t n) {
 
 	static const char map_bin2hex[] = "0123456789abcdef";
 
@@ -45,7 +45,7 @@ ssize_t bin2hex(const void *bin, char *hex, size_t n) {
  * @return On success this function returns the size of the binary data. If
  *   an error has occurred, -1 is returned and errno is set to indicate the
  *   error. */
-ssize_t hex2bin(const char *hex, void *bin, size_t n) {
+ssize_t hex2bin(const char * restrict hex, void * restrict bin, size_t n) {
 
 	static const int map_hex2bin[256] = {
 		['0'] = 0, 1, 2, 3, 4, 5, 6, 7, 8, 9,

src/shared/hex.h
@@ -1,6 +1,6 @@
 /*
  * BlueALSA - hex.h
- * Copyright (c) 2016-2021 Arkadiusz Bokowy
+ * Copyright (c) 2016-2024 Arkadiusz Bokowy
  *
  * This file is a part of bluez-alsa.
  *
@@ -18,7 +18,7 @@
 
 #include <sys/types.h>
 
-ssize_t bin2hex(const void *bin, char *hex, size_t n);
-ssize_t hex2bin(const char *hex, void *bin, size_t n);
+ssize_t bin2hex(const void * restrict bin, char * restrict hex, size_t n);
+ssize_t hex2bin(const char * restrict hex, void * restrict bin, size_t n);
 
 #endif

commit 59c953a2cd6ae81cfad9f3bf08b0905c3feb07ef

Fix audio scaling bug introduced in 45af1a7
Fixes #737

src/audio.c
@@ -84,7 +84,7 @@ void audio_scale_s16_2le(int16_t * restrict buffer,
 		const double * restrict scale, unsigned int channels, size_t frames) {
 	for (size_t i = 0; frames; frames--)
 		for (size_t c = 0; c < channels; c++, i++)
-			buffer[i] = htole16(((int16_t)le16toh(buffer[i]) * scale[c]));
+			buffer[i] = htole16((int16_t)((int16_t)le16toh(buffer[i]) * scale[c]));
 }
 
 /**
@@ -93,5 +93,5 @@ void audio_scale_s32_4le(int32_t * restrict buffer,
 		const double * restrict scale, unsigned int channels, size_t frames) {
 	for (size_t i = 0; frames; frames--)
 		for (size_t c = 0; c < channels; c++, i++)
-			buffer[i] = htole32((int32_t)le32toh(buffer[i]) * scale[c]);
+			buffer[i] = htole32((int32_t)((int32_t)le32toh(buffer[i]) * scale[c]));
 }