9786e53043b2d151f7e24bd5eb16211ae15d4c9f
borine <32966433+borine@users.noreply.github.com>
2025-07-30 17:42:51 +0100

    enable compilation of bluealsa-rfcomm with -std=gnu23

--- utils/rfcomm/rfcomm.c
+++ utils/rfcomm/rfcomm.c
@@ -254,7 +254,6 @@ usage:
 			if ((ret = read(rfcomm_fd, buffer, sizeof(buffer) - 1)) <= 0) {
 				if (output_is_tty) {
 					rl_replace_line("disconnected", 0);
-					rl_message("");
 					rl_redisplay();
 				}
 				break;
